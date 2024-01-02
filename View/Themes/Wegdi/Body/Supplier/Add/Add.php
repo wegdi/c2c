d77f6fc19b1755d5c597b683bfc2134613df44ac
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-02 17:17:18 +0300

    Update Add.php

--- View/Themes/Wegdi/Body/Supplier/Add/Add.php
+++ View/Themes/Wegdi/Body/Supplier/Add/Add.php
@@ -8,5 +8,17 @@ $security->LoginControl($guvenlik);
 
 
 <div class="row">
+  <div class="col-xl-6">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title mb-0">Default Scroll</h4>
+      </div><!-- end card header -->
 
+      <div class="card-body">
+        <p class="text-muted">Use <code>data-simplebar</code> attribute and add <code>max-height: **px</code> to set default scrollbar.</p>
+
+
+      </div><!-- end card-body -->
+    </div><!-- end card -->
+  </div>
 </div>
