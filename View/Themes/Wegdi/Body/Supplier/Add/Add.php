704b7352717d099aea6965fb53a19477493b6b6c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-02 17:22:21 +0300

    Update Add.php

--- View/Themes/Wegdi/Body/Supplier/Add/Add.php
+++ View/Themes/Wegdi/Body/Supplier/Add/Add.php
@@ -16,8 +16,8 @@ $security->LoginControl($guvenlik);
 
       <div class="card-body">
         <div class="row">
-          <div class="col-xxl-12 col-md-12">
-                <label for="exampleInputrounded" class="form-label text-center">Tedarikçi Adı</label>
+          <div class="col-xxl-12 col-md-12 text-center">
+                <label for="exampleInputrounded" class="form-label">Tedarikçi Adı</label>
                 <input type="text" class="form-control rounded-pill" id="exampleInputrounded" placeholder="Tedarikçi Adı Giriniz">
           </div>
         </div>
