840dd782e3977c03bb6a710ba5238934daf1f81a
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 16:44:51 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -4,7 +4,7 @@ require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 
 // JSON dosyasının URL'si
-$jsonUrl = 'https://c2c.wegdi.com/Json/65968ec9c3a67.json';
+$jsonUrl = 'https://c2c.wegdi.com/Json/65968acc2a290.json';
 
 // JSON verisini alma
 $jsonData = file_get_contents($jsonUrl);
@@ -36,8 +36,6 @@ foreach ($data as $ustAnahtar => $altDizi) {
 
     echo "<br>";
 }
-
-echo count($donguler);
 
 
 if (count($donguler)==1) {
