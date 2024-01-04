7ce2d16c52fd3a60f52fdfdc63018291b75cb868
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 20:32:26 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -12,11 +12,46 @@ $jsonData = file_get_contents($jsonUrl);
 // JSON verisini PHP dizisine dönüştürme
 $data = json_decode($jsonData, true);
 
-// Veritabanına kaydedilecek anahtarlar dizisi
+$donguler = [];
+$final = [];
+// Üst anahtarları yazdırma
+foreach ($data as $ustAnahtar => $altDizi) {
+    // Üst anahtarı string olarak almak istiyorsak
+    $ustAnahtarString = is_string($ustAnahtar) ? $ustAnahtar : json_encode($ustAnahtar);
+
+    echo "Üst Anahtar: " . $ustAnahtarString . "<br>";
+    $donguler[] = $ustAnahtarString;
+
+    // Alt diziyi yazdırma
+    foreach ($altDizi as $altAnahtar => $deger) {
+        // Alt anahtarı sadece int değilse yazdır
+        if (!is_int($altAnahtar)) {
+            // Alt anahtarı string olarak almak istiyorsak
+            $altAnahtarString = is_string($altAnahtar) ? $altAnahtar : json_encode($altAnahtar);
+
+            echo "    Alt Anahtar: " . $altAnahtarString . "<br>";
+            $donguler[] = $altAnahtarString;
+        }
+    }
+
+    echo "<br>";
+}
+
+if (count($donguler) == 1) {
+    $ilkDizi = reset($data["$donguler[0]"]);
+} elseif (count($donguler) == 2) {
+    $ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]);
+} elseif (count($donguler) == 3) {
+    $ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]);
+} elseif (count($donguler) == 4) {
+    $ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]["$donguler[3]"]);
+}
+
+
 $databaseKeys = [];
 
 // Fonksiyonu çağır ve veritabanı anahtarlarını al
-getDatabaseKeys($data);
+getDatabaseKeys($ilkDizi);
 
 // Veritabanına kaydedilecek anahtarları yazdır
 print_r($databaseKeys);
