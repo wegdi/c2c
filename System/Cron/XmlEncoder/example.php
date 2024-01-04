1e3192e7188a7a8797d481ad0d818e0ad213408c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 13:31:36 +0300

    Update example.php

--- System/Cron/XmlEncoder/example.php
+++ System/Cron/XmlEncoder/example.php
@@ -6,22 +6,6 @@ $xmlUrl = 'https://b2b.dogan-oto.com.tr/bayi/xmlexportv3Dogan.aspx?code=%C4%B0ST
 // XML verisini PHP SimpleXML nesnesine dönüştür
 $xml = simplexml_load_file($xmlUrl);
 
-echo $xml;
-// SimpleXML nesnesini JSON formatına çevir
-$json = json_encode($xml);
+print_r($xml);
 
-$json_decode=json_decode($json,1);
-// JSON verisini ekrana yazdır
-
-$i = 0;
-while($i <= 4){
-    echo $json_decode["stok"][$i]["marka"].' - '.$json_decode["stok"][$i]["kategori"].' - '.$json_decode["stok"][$i]["model"];
-    echo '<br>';
-    $i = $i+1;
-}
-/*
-echo '<pre>';
-print_r($json_decode["stok"]);
-echo '</pre>';*/
-echo 'stok son 4 adet';
 ?>
