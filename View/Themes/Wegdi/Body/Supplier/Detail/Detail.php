d02adb13aaed7554509de7f2eb6bd3948f0c04fa
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 17:33:07 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -2,6 +2,10 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
+
+
+
+
 // JSON dosyasının URL'si
 $jsonUrl = 'https://c2c.wegdi.com/Json/65968ec9c3a67.json';
 
@@ -11,8 +15,61 @@ $jsonData = file_get_contents($jsonUrl);
 // JSON verisini PHP dizisine dönüştürme
 $data = json_decode($jsonData, true);
 
-// Fonksiyonu çağırarak içeriği yazdırma
-printDiziIcerigi($data);
+$donguler = [];
+$final = [];
+// Üst anahtarları yazdırma
+foreach ($data as $ustAnahtar => $altDizi) {
+    // Üst anahtarı string olarak almak istiyorsak
+    $ustAnahtarString = is_string($ustAnahtar) ? $ustAnahtar : json_encode($ustAnahtar);
+
+    echo "Üst Anahtar: " . $ustAnahtarString . "<br>";
+    $donguler[] = $ustAnahtarString;
+
+    // Alt diziyi yazdırma
+    foreach ($altDizi as $altAnahtar => $deger) {
+        // Alt anahtarı sadece int değilse yazdır
+        if (!is_int($altAnahtar)) {
+            // Alt anahtarı string olarak almak istiyorsak
+            $altAnahtarString = is_string($altAnahtar) ? $altAnahtar : json_encode($altAnahtar);
+
+            echo "    Alt Anahtar: " . $altAnahtarString . "<br>";
+            $donguler[] = $altAnahtarString;
+        }
+    }
+
+    echo "<br>";
+}
+
+
+if (count($donguler)==1) {
+
+  $ilkDizi = reset($data["$donguler[0]"]);
+  foreach ($ilkDizi as $keyx => $valuex) {
+    echo $keyx.' -- ';
+    echo "<br>";
+  }
+
+}elseif (count($donguler)==2) {
+
+$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]);
+foreach ($ilkDizi as $keyx => $valuex) {
+  echo $keyx.' -- ';
+  echo "<br>";
+}
+}elseif (count($donguler)==3) {
+$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]);
+foreach ($ilkDizi as $keyx => $valuex) {
+  echo $keyx.' -- ';
+}
+}elseif (count($donguler)==4) {
+$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]["$donguler[3]"]);
+foreach ($ilkDizi as $keyx => $valuex) {
+  echo $keyx.' -- ';
+}
+}
+
+
+printDiziIcerigi($ilkDizi);
 
 // Fonksiyonun tanımı
 function printDiziIcerigi($veri, $indent = 0) {
