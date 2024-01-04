e71d5bc68ba8da225349d074e8d8a855a487886d
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 15:11:00 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -3,9 +3,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 
-
-
-
 // JSON dosyasının URL'si
 $jsonUrl = 'https://c2c.wegdi.com/Json/659694796d1cf.json';
 
@@ -15,15 +12,14 @@ $jsonData = file_get_contents($jsonUrl);
 // JSON verisini PHP dizisine dönüştürme
 $data = json_decode($jsonData, true);
 
-
 // İlk elemanı al
 $ilkEleman = null;
 
-if (!empty($data['stok']) && is_array($data['stok'])) {
-    $ilkEleman = $data['stok'][0];
+if (!empty($data) && is_array($data)) {
+    reset($data); // Diziyi sıfırla (başa konumlandır)
+    $ilkEleman = current($data); // İlk elemanı al
 }
 
 // PHP dizisini ekrana yazdırma
 print_r($ilkEleman);
-
 ?>
