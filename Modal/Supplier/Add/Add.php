8f1d89ede997dacd07f521065f7d3fda12417a40
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 13:17:18 +0300

    Update Add.php

--- Modal/Supplier/Add/Add.php
+++ Modal/Supplier/Add/Add.php
@@ -3,29 +3,29 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 require_once(SYSTEM.'General/General.php');
-$db=new General();
-$uniqid=uniqid();
+$db = new General();
+$uniqid = uniqid();
 
 if ($_POST["tedarikciLink"]!="" and $_POST["tedarikciLink"]!="") {
 
-  // XML verisini çekmek için URL
-  $xmlUrl = 'https://b2b.dogan-oto.com.tr/bayi/xmlexportv3Dogan.aspx?code=%C4%B0STANBUL.0631';
-
   // XML verisini PHP SimpleXML nesnesine dönüştür
-  $xml = simplexml_load_file($xmlUrl);
+  $xml = simplexml_load_file($_POST["tedarikciLink"]);
 
   // SimpleXML nesnesini JSON formatına çevir
-  $json = json_encode($xml);
+  $json = json_encode($xml, JSON_PRETTY_PRINT);
 
-  $json_decode=json_decode($json,1);
+  // JSON dosyasını belirli bir dizine kaydet
+  $jsonFilePath = JSONFILE.$uniqid.'.json'; // Burada "your_filename.json" dosya adını değiştirebilirsiniz.
+  file_put_contents($jsonFilePath, $json);
+
+  $json_decode = json_decode($json, true);
 
   $data = array(
       'SupplierName' => $db->Guvenlik($_POST["tedarikciAdi"]),
       'SupplierUrl' => $db->Guvenlik($_POST["tedarikciLink"]),
       'SupplierCode' => $uniqid,
-
   );
 
   echo $db->Add("Supplier", $data);
-
 }
+?>
