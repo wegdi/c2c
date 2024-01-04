77009d57daa0a5fed2d0f3f474782af5a1d86794
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 20:03:12 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -38,7 +38,7 @@ foreach ($data as $ustAnahtar => $altDizi) {
 }
 
 if (count($donguler) == 1) {
-    $ilkDizi = reset($data["$donguler[0]"]);
+    $ilkDizi = end($data["$donguler[0]"]);
 } elseif (count($donguler) == 2) {
     $ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]);
 } elseif (count($donguler) == 3) {
