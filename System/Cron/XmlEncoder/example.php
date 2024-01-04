514a5bfefc2f8714f8c72cdb971d2f867d4814b8
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 13:33:22 +0300

    Update example.php

--- System/Cron/XmlEncoder/example.php
+++ System/Cron/XmlEncoder/example.php
@@ -3,9 +3,18 @@
 // XML verisini çekmek için URL
 $xmlUrl = 'https://b2b.dogan-oto.com.tr/bayi/xmlexportv3Dogan.aspx?code=%C4%B0STANBUL.0631';
 
-// XML verisini PHP SimpleXML nesnesine dönüştür
-$xml = simplexml_load_file($xmlUrl);
+// XML verisini indir
+$xmlData = file_get_contents($xmlUrl);
 
+// Eğer başarılı bir şekilde indirilemezse, hata mesajını göster
+if ($xmlData === FALSE) {
+    die('XML verisi indirilemedi.');
+}
+
+// XML verisini PHP SimpleXML nesnesine dönüştür
+$xml = simplexml_load_string($xmlData);
+
+// Elde edilen SimpleXML nesnesini göster
 print_r($xml);
 
 ?>
