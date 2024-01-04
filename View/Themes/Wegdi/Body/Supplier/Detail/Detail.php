a03116be7d5e6a55c29b9670017ad67e9e131d2b
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 19:38:45 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -4,7 +4,7 @@ require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 
 // JSON dosyasının URL'si
-$jsonUrl = 'https://c2c.wegdi.com/Json/65968ec9c3a67.json';
+$jsonUrl = 'https://c2c.wegdi.com/Json/659687e783c65.json';
 
 // JSON verisini alma
 $jsonData = file_get_contents($jsonUrl);
