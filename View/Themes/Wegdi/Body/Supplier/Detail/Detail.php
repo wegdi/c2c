e3143e97a1944752cc33eccd60d33909dc99009a
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 20:35:40 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -64,11 +64,7 @@ function getDatabaseKeys($veri, $parentKey = null) {
 
     foreach ($veri as $anahtar => $deger) {
         $currentKey = ($parentKey) ? $parentKey . ' -> ' . $anahtar : $anahtar;
-        $currentValue = $deger;
-
         $databaseKeys[] = $currentKey;
-        $databaseKeys[]= $currentValue;
-
 
         if (is_array($deger)) {
             getDatabaseKeys($deger, $currentKey);
