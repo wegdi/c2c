b2776c215ff905299987a2183ed7425dd4dc6bfc
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 13:39:00 +0300

    Update Add.php

--- Modal/Supplier/Add/Add.php
+++ Modal/Supplier/Add/Add.php
@@ -1,4 +1,11 @@
 <?php
+function get_cdata($element) {
+    $node = dom_import_simplexml($element);
+    $cdata = $node->ownerDocument->createCDATASection($node->textContent);
+    $node->replaceChild($cdata, $node->firstChild);
+    return $element;
+}
+
 require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
@@ -6,30 +13,27 @@ require_once(SYSTEM.'General/General.php');
 $db = new General();
 $uniqid = uniqid();
 
-print_r($_POST);
 if ($_POST["tedarikciAdi"]!="" and $_POST["tedarikciLink"]!="") {
 
-  // XML verisini PHP SimpleXML nesnesine dönüştür
-  //$xml = simplexml_load_file($_POST["tedarikciLink"]);
+    // XML verisini PHP SimpleXML nesnesine dönüştür
+    $xml = simplexml_load_file($_POST["tedarikciLink"]);
 
-  $xml = file_get_contents($_POST["tedarikciLink"]);
+    // CDATA içeriklerini elde etmek için XML içeriğini döngü ile gezip get_cdata fonksiyonunu kullan
+    array_walk_recursive($xml, 'get_cdata');
 
+    $json = json_encode($xml, JSON_PRETTY_PRINT);
 
-  $json = json_encode($xml, JSON_PRETTY_PRINT);
+    $jsonFilePath = JSONFILE.$uniqid.'.json';
+    echo $jsonFilePath;
+    file_put_contents($jsonFilePath, $json);
 
-  $jsonFilePath = JSONFILE.$uniqid.'.json';
-  echo $jsonFilePath;
-  file_put_contents($jsonFilePath, $json);
+    $data = array(
+        'SupplierName' => $db->Guvenlik($_POST["tedarikciAdi"]),
+        'SupplierUrl' => $db->Guvenlik($_POST["tedarikciLink"]),
+        'SupplierCode' => $uniqid,
+        'SupplierFilePath' => '/Json/'.$uniqid.'.json'
+    );
 
-
-  $data = array(
-      'SupplierName' => $db->Guvenlik($_POST["tedarikciAdi"]),
-      'SupplierUrl' => $db->Guvenlik($_POST["tedarikciLink"]),
-      'SupplierCode' => $uniqid,
-      'SupplierFilePath' => '/Json/'.$uniqid.'.json'
-
-  );
-
-  //echo $db->Add("Supplier", $data);
+    //echo $db->Add("Supplier", $data);
 }
 ?>
