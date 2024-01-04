2613829de5b4d9b3e3520a31a2e871333cf2b0c5
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 16:49:21 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -12,58 +12,36 @@ $jsonData = file_get_contents($jsonUrl);
 // JSON verisini PHP dizisine dönüştürme
 $data = json_decode($jsonData, true);
 
-$donguler = [];
-$final = [];
-// Üst anahtarları yazdırma
+// Tüm key'leri toplamak için kullanılacak dizi
+$tumKeyler = [];
+
+// Key'leri toplama fonksiyonu
+function toplaKeyler($veri, &$keyler) {
+    foreach ($veri as $key => $value) {
+        $keyler[] = $key;
+        if (is_array($value)) {
+            toplaKeyler($value, $keyler);
+        }
+    }
+}
+
+// Üst anahtarları yazdırma ve key'leri toplama
 foreach ($data as $ustAnahtar => $altDizi) {
     // Üst anahtarı string olarak almak istiyorsak
     $ustAnahtarString = is_string($ustAnahtar) ? $ustAnahtar : json_encode($ustAnahtar);
 
     echo "Üst Anahtar: " . $ustAnahtarString . "<br>";
-    $donguler[] = $ustAnahtarString;
+    $tumKeyler[] = $ustAnahtarString;
 
-    // Alt diziyi yazdırma
-    foreach ($altDizi as $altAnahtar => $deger) {
-        // Alt anahtarı sadece int değilse yazdır
-        if (!is_int($altAnahtar)) {
-            // Alt anahtarı string olarak almak istiyorsak
-            $altAnahtarString = is_string($altAnahtar) ? $altAnahtar : json_encode($altAnahtar);
-
-            echo "    Alt Anahtar: " . $altAnahtarString . "<br>";
-            $donguler[] = $altAnahtarString;
-        }
-    }
+    // Alt diziyi yazdırma ve key'leri toplama
+    toplaKeyler($altDizi, $tumKeyler);
 
     echo "<br>";
 }
 
-
-if (count($donguler)==1) {
-
-  $ilkDizi = reset($data["$donguler[0]"]);
-  foreach ($ilkDizi as $keyx => $valuex) {
-    echo $keyx.' -- ';
-    echo "<br>";
-  }
-
-}elseif (count($donguler)==2) {
-
-$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]);
-foreach ($ilkDizi as $keyx => $valuex) {
-  echo $keyx.' -- ';
-  echo "<br>";
+// Tüm key'leri ekrana yazdırma
+echo "Tüm Key'ler: <br>";
+foreach ($tumKeyler as $key) {
+    echo $key . "<br>";
 }
-}elseif (count($donguler)==3) {
-$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]);
-foreach ($ilkDizi as $keyx => $valuex) {
-  echo $keyx.' -- ';
-}
-}elseif (count($donguler)==4) {
-$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]["$donguler[3]"]);
-foreach ($ilkDizi as $keyx => $valuex) {
-  echo $keyx.' -- ';
-}
-}
-
-
 ?>
