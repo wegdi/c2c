7a7b596b47ff31e58935dcc31574fe5ca15ddb39
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-04 16:36:48 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -51,7 +51,10 @@ if (count($donguler)==1) {
 
 }elseif (count($donguler)==2) {
   foreach ($data["$donguler[0]"]["$donguler[1]"] as $key => $value) {
-      print_r($value);
+    if (!empty($value)) {
+        // PHP dizisinin ilk elemanını alın
+        $ilkDeger = reset($value);
+    }
   }
 
 }
