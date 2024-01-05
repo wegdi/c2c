7a9d4f04b4c66fb8631ae4f3d172d30729569251
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-05 16:32:40 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -35,7 +35,17 @@ foreach ($data as $ustAnahtar => $altDizi) {
 if (count($donguler) == 1) {
     $ilkDizi = reset($data["$donguler[0]"]);
 } elseif (count($donguler) == 2) {
-    $ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]);
+  foreach ($donguler as $index => $dongu) {
+    if ($index === 2) {
+        // Ortadaki elemanın indeksindeyiz, bu yüzden güncel $ilkDizi'yi alıyoruz
+        $ilkDizi = $ilkDizi[$dongu];
+    }
+    // Eğer $ortaIndex -1 ise, $dongulerCount çifttir ve orta eleman yoktur.
+    // Eğer $ortaIndex 0 ise, sadece bir eleman vardır ve bu da zaten ortadır.
+    // Dolayısıyla $ilkDizi herhangi bir güncelleme almayacaktır.
+}
+
+    //$ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]);
 } elseif (count($donguler) == 3) {
     $ilkDizi = reset($data["$donguler[0]"]["$donguler[1]"]["$donguler[2]"]);
 } elseif (count($donguler) == 4) {
