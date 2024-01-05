d465e4a30ec1dfed5e602de08767f0d5cea142a7
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-05 17:49:03 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -152,4 +152,14 @@ echo $tableHtml;
 ?>
 
 
+<div class="col-lg-3">
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title mb-0">Xml Ön İzleme</h5>
+        </div>
+        <div class="card-body">
+
+</div>
+</div>
+</div>
 </div>
