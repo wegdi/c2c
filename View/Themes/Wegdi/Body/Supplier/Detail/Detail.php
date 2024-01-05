c28d03d20e50e16349e661ece00bfc3766f16eee
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-05 17:59:54 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -192,7 +192,7 @@ $options = [
                 <th><?php echo $value; ?></th>
                 <th>
                   <?php if ($jsonUrl["$key"]): ?>
-                      var
+                    <i class="ri-checkbox-circle-fill "></i> 
                       <?php else: ?>
                         yok
                   <?php endif; ?>
