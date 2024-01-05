8d36479efa41d1f717ca0507edba899aee287e58
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-05 17:23:53 +0300

    Update Detail.php

--- Modal/Supplier/Detail/Detail.php
+++ Modal/Supplier/Detail/Detail.php
@@ -2,4 +2,24 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
+require_once(SYSTEM.'General/General.php');
+$db=new General();
+
+$SupplierTag = $db->Query('Supplier',["SupplierCode" => (string)$_POST["SupplierID"]], [], 'TEK');
+
+if ($SupplierTag["_id"]!="") {
+
+  $data = array(
+      'Tag' => [
+        'TagName' => $_POST["selectedValue"],
+        'TagValue' => $_POST["tag"],
+
+      ]
+  );
+  echo $db->UpdateByObjectId("Supplier",(string)$SupplierTag["_id"], $data);
+
+}
+
+
+
 ?>
