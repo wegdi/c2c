552965261faa247c0ce418a4adff99ef40b2f5ea
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-05 11:41:14 +0300

    Update example.php

--- System/Cron/XmlEncoder/example.php
+++ System/Cron/XmlEncoder/example.php
@@ -18,8 +18,8 @@ $db = new General();
 $uniqid = uniqid();
 
 
+    //1. kategori bilgileri
     $domain = 'https://www.onlineyedekparca.com';
-
     preg_match_all('/data-selector="first-level-navigation".*?<a\s+href="(.*?)".*?title="(.*?)"/s', file_get_contents("$domain"), $kategori);
     $r = 0;
     while($r< count($kategori[1])){
@@ -32,11 +32,8 @@ $uniqid = uniqid();
                 'Title' => $db->Guvenlik($kategori[2][$r])
             );
             $db->Add("Category_Menu", $data);
+            //2. kategori bilgileri
             $domain2 = $domain.'/kategori/opel-yedek-parca';
-            //preg_match_all('/<a\s+href="([^"]+)"\s+title="([^"]+)"[^>]*>(.*?)<\/a>/s', file_get_contents("$domain2"), $kategori2);
-            //echo '<pre>';
-            //print_r($kategori2[1]);
-            //echo '</pre>';
             $dom = new DOMDocument();
             $dom->loadHTML(file_get_contents("$domain2"));
             $finder = new DomXPath($dom);
@@ -53,6 +50,24 @@ $uniqid = uniqid();
                         'Title' => $db->Guvenlik($kategori2_item->getAttribute('title'))
                     );
                     $db->Add("Category_Menu", $data2);
+                    //son kategori bilgileri
+                    $domain3 = $domain.$kategori2_item->getAttribute('href');
+                    $dom3 = new DOMDocument();
+                    $dom3->loadHTML(file_get_contents("$domain3"));
+                    $finder3 = new DomXPath($dom3);
+                    $classname = "filter-menu-category-content";
+                    $kategori3 = $finder3->query("//*[contains(@class, '$classname')]//a");
+                    $t = 0;
+                    foreach ($kategori3 as $kategori3_item) {
+                        $uniqid3 = uniqid();
+                        $data3 = array(
+                            'Uniqid' => $uniqid3,
+                            'GroupId'=> $uniqid2,
+                            'Url' => $kategori3_item->getAttribute('href'),
+                            'Title' => $db->Guvenlik($kategori3_item->getAttribute('title'))
+                        );
+                        $db->Add("Category_Menu", $data3);
+                    }
                 }
                 $k = $k+1;
             }
