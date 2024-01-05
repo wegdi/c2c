6298e860e732c3ecbd1d007593603ff696fcad00
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-05 11:29:51 +0300

    Update example.php

--- System/Cron/XmlEncoder/example.php
+++ System/Cron/XmlEncoder/example.php
@@ -43,7 +43,7 @@ $uniqid = uniqid();
             $classname = "filter-menu-category-content";
             $kategori2 = $finder->query("//*[contains(@class, '$classname')]//a");
             echo '<pre>';
-            print_r($kategori2);
+            print_r($kategori2->getAttribute('href'));
             echo '</pre>';
             $k = 0;
             /*
