f9ca535cedcdd3143e56f552edab845d9fe35d8f
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-05 16:07:12 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -69,7 +69,7 @@ $tableHtml = '
   <div class="col-lg-12">
       <div class="card">
           <div class="card-header">
-              <h5 class="card-title mb-0"></h5>
+              <h5 class="card-title mb-0">Xml Eşleştirme Tablosu</h5>
           </div>
           <div class="card-body">
 <table class="display table table-bordered dt-responsive">';
