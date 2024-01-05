e4a6ef65861af67f2deb9218d740473255118457
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-05 22:05:03 +0300

    Update Detail.php

--- View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
+++ View/Themes/Wegdi/Body/Supplier/Detail/Detail.php
@@ -5,7 +5,7 @@
   }
 </style>
 
-<div class="alert alert-success alert-top-border alert-dismissible fade hide" role="alert">
+<div class="alert alert-success alert-top-border alert-dismissible fade d-none" role="alert">
     <i class="ri-notification-off-line me-3 align-middle fs-16 text-success"></i><strong>Success</strong> - Top border alert
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
