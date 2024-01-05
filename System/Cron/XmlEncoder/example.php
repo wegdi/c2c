1e3024d5fd7a42daa77721a494fd11b5776357e2
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-05 10:58:23 +0300

    Update example.php

--- System/Cron/XmlEncoder/example.php
+++ System/Cron/XmlEncoder/example.php
@@ -18,52 +18,22 @@ $db = new General();
 $uniqid = uniqid();
 
 
+    $domain = 'https://www.onlineyedekparca.com/';
 
-    preg_match_all('/"(.+)": "(.+)"/', file_get_contents("https://c2c.wegdi.com/Json/659693f87cc90.json"), $output_array);
+    preg_match_all('/data-selector="first-level-navigation".*?<a\s+href="(.*?)".*?title="(.*?)"/s', file_get_contents("$domain"), $kategori);
     $r = 0;
-    while($r<= count($output_array[1])){
-        $data = array(
-            'LeftData' => $db->Guvenlik($output_array[1][$r]),
-            'RightData' => $db->Guvenlik($output_array[2][$r])
-        );
-        $db->Add("RCategory", $data);
+    while($r<= count($kategori[1])){
+        if($r <= 1){
+            $uniqid = uniqid();
+            $data = array(
+                'Uniqid' => $uniqid,
+                'Title' => $db->Guvenlik($kategori[1][$r]),
+                'Url' => $db->Guvenlik($kategori[2][$r])
+            );
+            $db->Add("Category_Menu", $data);
+        }
         $r = $r+1;
     }
     echo 'bitti';
-
-?>
-
-
-
-
-<?php
-
-// XML verisini çekmek için URL
-//$xmlUrl = 'https://b2b.dogan-oto.com.tr/bayi/xmlexportv3Dogan.aspx?code=%C4%B0STANBUL.0631';
-
-// XML verisini indir
-//$xmlData = file_get_contents($xmlUrl);
-//echo $xmlData;
-//print_r($xmlData);
-/*
-$jsonFilePath = JSONFILE.$uniqid.'.json';
-
-
-file_put_contents($jsonFilePath, $xmlData);
-
-// Eğer başarılı bir şekilde indirilemezse, hata mesajını göster
-if ($xmlData === FALSE) {
-    die('XML verisi indirilemedi.');
-}
-
-// XML verisini PHP SimpleXML nesnesine dönüştür
-$xml = simplexml_load_string($xmlData);
-
-// Elde edilen SimpleXML nesnesini göster
-print_r($xml);
-*/
-
-
-
 
 ?>
