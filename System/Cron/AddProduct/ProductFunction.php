e775b111892b353e5e463069a84816dab7555497
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 19:54:53 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -53,10 +53,7 @@ class ProductJsonDecoder {
             $product_nameValue = $valuedecodedData[$this->ProductJsonLoginEnd($istek)];
 
             // $ProductData dizisine ekleme
-            $ProductData[] = array(
-              'model' => $modelValue,
-              'product_name' => $product_nameValue
-             );
+            $ProductData[] = array('model' => $modelValue, $tagname => $product_nameValue);
         }
     }elseif ($model == 3) {
       $explode = explode(';', $modelv);
