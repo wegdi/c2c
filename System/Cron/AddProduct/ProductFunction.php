aae5f2ffde2fb34f78060d705e5d037df7530a7c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 17:53:15 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -33,6 +33,31 @@ class ProductJsonDecoder {
 
   }
 
+  public function ReturnProduct($url='',$value='')
+  {
+    $model = $this->ProductJsonLoginCount($value["model"]);
+
+    $jsonData = file_get_contents($url);
+    $decodedData = json_decode($jsonData, true);
+
+    $ProductData = [];
+
+    if ($model == 2) {
+        $explode = explode(';', $value["model"]);
+        $one = $explode[0];
+        $two = $explode[1];
+
+        foreach ($decodedData[$one] as $keydecodedData => $valuedecodedData) {
+            // Ekrana sıralı bir şekilde yazdırma
+            $modelValue = $valuedecodedData[$this->ProductJsonLoginEnd($value["model"])];
+            $product_nameValue = $valuedecodedData[$this->ProductJsonLoginEnd($value["product_name"])];
+
+            // $ProductData dizisine ekleme
+            $ProductData[] = array('model' => $modelValue, 'product_name' => $product_nameValue);
+        }
+    }
+  }
+
 
 }
 
