c0246c8c2deae85d8c0d5f6ad9958ea822a8afdc
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 22:35:59 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -43,13 +43,13 @@ class ProductJsonDecoder {
               $start = 0;
               $part = ceil(count($decodedData[$one]) / 10);
           } else {
-              $start = ceil(count($decodedData[$one]) / 10);
+              $start = ceil(count($decodedData[$one]) / 10)*$gettotal;
               $part = ceil(count($decodedData[$one]) / 10);
           }
 
 
             echo $start;
-          
+
             $bol = array_slice($decodedData[$one], 1, $part);
           //  print_r($bol);
             foreach ($decodedData[$one] as $keydecodedData => $valuedecodedData) {
