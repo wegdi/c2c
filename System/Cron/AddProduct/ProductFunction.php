82f23bee06c77f19bc31be37acc450814e0a8c45
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 21:45:50 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -50,9 +50,10 @@ class ProductJsonDecoder {
         foreach ($decodedData[$one] as $keydecodedData => $valuedecodedData) {
           $modelValue = $valuedecodedData[$this->ProductJsonLoginEnd($modelv)];
           foreach ($istek as $istekler) {
-            $product_{$istekler} = $valuedecodedData[$this->ProductJsonLoginEnd($istekler)];
-            echo $product_{$istekler};
-          }
+          $product_$istekler = $valuedecodedData[$this->ProductJsonLoginEnd($istekler)];
+          echo $product_$istekler;
+      }
+
           //print_r($istek);
 
             // Ekrana sıralı bir şekilde yazdırma
