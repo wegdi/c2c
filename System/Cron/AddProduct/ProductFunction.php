7bd4b545934dd24fb424765618aa34e2cafdfe3f
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 15:19:42 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -13,10 +13,15 @@ class ProductJsonDecoder {
 
     if ($count==2) {
       $jsonData = file_get_contents("https://c2c.wegdi.com".$SupplierFilePath);
+      $one=$explode[0];
+      $two=$explode[1]
+      $array =[];
+      foreach ($jsonData["$one"] as $key => $value) {
 
-      foreach ($jsonData as $key => $value) {
+        $array=[]=$value["$two"];
 
       }
+      return $array;
     }
 
   }
