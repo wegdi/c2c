21b619b469d5610d8177bdaf0ca5b0d55a809797
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 15:44:42 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -12,7 +12,7 @@ $Supplier = $db->Query('Supplier',["Status" =>1], [], 'COK');
 
 $ProductData=[];
 foreach ($Supplier as $key => $value) {
-    print_r($value);
+    print_r($key);
 
     if (isset($value["model"])) {
 
