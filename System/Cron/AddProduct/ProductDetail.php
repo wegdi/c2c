318a63761517135d410e02fc049b262f2d3a6758
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 15:26:07 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -14,14 +14,11 @@ $Supplier = $db->Query('Supplier',["Status" =>1], [], 'COK');
 foreach ($Supplier as $key => $value) {
 
     if (isset($value["model"])) {
-    echo "string";
-
-
-  $dd=  array_merge($Product->ProductJsonLogin($value["model"],$value["SupplierFilePath"]), $Product->ProductJsonLogin($value["product_name"],$value["SupplierFilePath"]));
+    print_r(  $Product->ProductJsonLogin($value["model"],$value["SupplierFilePath"]));
+    print_r(  $Product->ProductJsonLogin($value["product_name"],$value["SupplierFilePath"]));
 
     }
 
 }
-print_r(   $dd );
 
  ?>
