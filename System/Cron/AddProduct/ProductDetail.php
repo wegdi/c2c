7c660eb98bbe4f1cdce807e31e6af8dfb97852b6
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 17:48:00 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -8,53 +8,40 @@ require_once('ProductFunction.php');
 $db = new General();
 $Product = new ProductJsonDecoder();
 
-
-
-
 $Supplier = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 
 foreach ($Supplier as $key => $value) {
-
-
-
-   $model=$Product->ProductJsonLoginCount($value["model"]);
-   $product_name=$Product->ProductJsonLoginCount($value["product_name"]);
+   $model = $Product->ProductJsonLoginCount($value["model"]);
+   $product_name = $Product->ProductJsonLoginCount($value["product_name"]);
 
     $jsonData = file_get_contents(URL.$value["SupplierFilePath"]);
     $decodedData = json_decode($jsonData, true);
 
+    $ProductData = [];
 
     if ($model == 2) {
-        $explode=explode(';',$value["model"]);
+        $explode = explode(';', $value["model"]);
         $one = $explode[0];
         $two = $explode[1];
-        $ProductData = [];
 
         foreach ($decodedData[$one] as $keydecodedData => $valuedecodedData) {
             // Ekrana sıralı bir şekilde yazdırma
-            $ProductData['model']=$valuedecodedData[$Product->ProductJsonLoginEnd($value["model"])];
-            $ProductData['product_name']=$valuedecodedData[$Product->ProductJsonLoginEnd($value["product_name"])];
-
+            $ProductData['model'] = $valuedecodedData[$Product->ProductJsonLoginEnd($value["model"])];
+            $ProductData['product_name'] = $valuedecodedData[$Product->ProductJsonLoginEnd($value["product_name"])];
 
             echo "<br>";
         }
-        print_R($ProductData);
-    }elseif ($model == 3) {
-      $explode=explode(';',$value["model"]);
-      $one = $explode[0];
-      $two = $explode[1];
-      $tree = $explode[2];
+    } elseif ($model == 3) {
+        $explode = explode(';', $value["model"]);
+        $one = $explode[0];
+        $tree = $explode[2];
 
-      $ProductData = [];
-
-      foreach ($decodedData[$one][$tree] as $keydecodedData => $valuedecodedData) {
-          // Ekrana sıralı bir şekilde yazdırma
-          echo 'model'.'-->'.$valuedecodedData["$tree"];
-
-      }
+        foreach ($decodedData[$one][$tree] as $keydecodedData => $valuedecodedData) {
+            // Ekrana sıralı bir şekilde yazdırma
+            echo 'model'.'-->'.$valuedecodedData["$tree"];
+        }
     }
 
-
-
+    print_r($ProductData); // Döngü içinde değil, döngü sonunda çağrılmalıdır.
 }
 ?>
