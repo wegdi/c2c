f8c57a319923f62d71734ae1a3192fe5e90c8702
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 16:35:39 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -8,13 +8,6 @@ require_once('ProductFunction.php');
 $db = new General();
 $Product = new ProductJsonDecoder();
 
-$QuerList = array(
-    "product_name",
-    "product_description",
-    "product_meta_description",
-    "product_meta_keyword",
-    "model"
-);
 
 
 
@@ -23,7 +16,24 @@ $Supplier = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 foreach ($Supplier as $key => $value) {
 
     $model=$Product->ProductJsonLoginCount($value["model"]);
-    echo $model;
+    $product_description=$Product->ProductJsonLoginCount($value["product_description"]);
+    $product_meta_description=$Product->ProductJsonLoginCount($value["product_meta_description"]);
+    $product_meta_keyword=$Product->ProductJsonLoginCount($value["product_meta_keyword"]);
+    $product_meta_keyword=$Product->ProductJsonLoginCount($value["product_meta_keyword"]);
+    $sku=$Product->ProductJsonLoginCount($value["sku"]);
+    $quantity=$Product->ProductJsonLoginCount($value["quantity"]);
+    $kdv=$Product->ProductJsonLoginCount($value["kdv"]);
+    $price=$Product->ProductJsonLoginCount($value["price"]);
+
+    $jsonData = file_get_contents(URL.$value["SupplierFilePath"]);
+    $decodedData = json_decode($jsonData, true);
+
+
+        if (isset($value["model"])) {
+          // code...
+        }
+
+
 
 }
 ?>
