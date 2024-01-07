bb6799971579680162d29e79c63524e11e2d0bb0
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 18:09:42 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -15,7 +15,7 @@ $Supplier = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 foreach ($Supplier as $key => $value) {
 
 
-  $ss=$Product->ReturnProduct(URL.$value["SupplierFilePath"],$value["model"],$value["product_name"],'omu');
+  $ss=$Product->ReturnProduct(URL.$value["SupplierFilePath"],$value["model"],$value["product_name"],'product_name');
   print_r($ss);
 
 }
