3f9a734c5f013968ead41c7bec0e2aa4ea32040a
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 14:24:48 +0300

    Update ProductDetail.php

---
+++ System/Cron/AddProduct/ProductDetail.php
@@ -0,0 +1,9 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SECURITY.'Security.php');
+$security->LoginControl($guvenlik);
+require_once(SYSTEM.'General/General.php');
+$db = new General();
+
+
+ ?>
