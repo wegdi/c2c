1f2da003b592b5fc92ed365cba1bb3c31ce45ca1
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 15:21:41 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -5,25 +5,31 @@ class ProductJsonDecoder {
 
 
 
-  public function  ProductJsonLogin($value='',$SupplierFilePath='')
+  public function ProductJsonLogin($value = '', $SupplierFilePath = '')
   {
-    $explode= explode(';',$value);
-    $count=count($explode);
-  
-    if ($count==2) {
-      $jsonData = file_get_contents("https://c2c.wegdi.com".$SupplierFilePath);
-      $one=$explode[0];
-      $two=$explode[1]
-      $array =[];
-      foreach ($jsonData["$one"] as $key => $value) {
+      $explode = explode(';', $value);
+      $count = count($explode);
 
-        $array=[]=$value["$two"];
+      if ($count == 2) {
+          $jsonData = file_get_contents("https://c2c.wegdi.com" . $SupplierFilePath);
+          $one = $explode[0];
+          $two = $explode[1];
+          $array = [];
 
+          $decodedData = json_decode($jsonData, true);
+
+          if (isset($decodedData[$one])) {
+              foreach ($decodedData[$one] as $key => $value) {
+                  if (isset($value[$two])) {
+                      $array[] = $value[$two];
+                  }
+              }
+          }
+
+          return $array;
       }
-      return $array;
-    }
-
   }
+
 
 }
 
