5018d216f08af159c6f7af11be06b3bbb9349142
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-07 14:41:52 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -3,7 +3,10 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 require_once(SYSTEM.'General/General.php');
+require_once('ProductFunction.php');
+
 $db = new General();
+$Product = new ProductJsonDecoder();
 
 $Supplier = $db->Query('Supplier',["Status" =>1], [], 'COK');
 
