41d3ff24fab4b67d77898d6c205cfc7a683040d0
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-11 20:06:54 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -24,43 +24,44 @@ foreach ($Supplier as $key => $value) {
 
     if (count($explode) == 1) {
       foreach ($decodedData[$explode[0]] as $keyUrun => $valueUrun) {
-        print_R($valueUrun);
+
+        $Urunler = [];
+
+        $productFields = [
+          "product_name", "product_description", "product_meta_description", "product_meta_keyword",
+          "model", "sku", "quantity", "main_image", "image_1", "image_2", "image_3", "image_4", "image_5",
+          "image_6", "image_7", "image_8", "image_9", "image_10", "manufacturer_name", "price",
+          "product_option_price", "product_option_quantity", "product_option_name", "product_option_value",
+          "product_attribute_group", "product_attribute_name", "product_attribute_value", "kdv"
+       ];
+
+       foreach ($fields as $field) {
+           $fieldArray = Parcala($value[$field]);
+
+           if (count($fieldArray) == 1) {
+               $Urunler[$field] = $valueUrun[$fieldArray[0]];
+           } elseif (count($fieldArray) == 3) {
+               foreach ($valueUrun[$fieldArray[0]] as $keyBirAlt => $valueBirAlt) {
+                   $Urunler[$field] = $valueBirAlt[end($fieldArray)];
+               }
+           }
+       }
+
+
       }
     }elseif (count($explode) == 2) {
         foreach ($decodedData[$explode[0]] as $keyUrun => $valueUrun) {
             foreach ($valueUrun as $keyUrunIC => $valueUrunIC) {
                 $Urunler = [];
 
-                $fields = [
-                            "product_name",
-                            "product_description",
-                            "product_meta_description",
-                            "product_meta_keyword",
-                            "model",
-                            "sku",
-                            "quantity",
-                            "main_image",
-                            "image_1",
-                            "image_2",
-                            "image_3",
-                            "image_4",
-                            "image_5",
-                            "image_6",
-                            "image_7",
-                            "image_8",
-                            "image_9",
-                            "image_10",
-                            "manufacturer_name",
-                            "price",
-                            "product_option_price",
-                            "product_option_quantity",
-                            "product_option_name",
-                            "product_option_value",
-                            "product_attribute_group",
-                            "product_attribute_name",
-                            "product_attribute_value",
-                            "kdv",
-                          ];
+                $productFields = [
+                  "product_name", "product_description", "product_meta_description", "product_meta_keyword",
+                  "model", "sku", "quantity", "main_image", "image_1", "image_2", "image_3", "image_4", "image_5",
+                  "image_6", "image_7", "image_8", "image_9", "image_10", "manufacturer_name", "price",
+                  "product_option_price", "product_option_quantity", "product_option_name", "product_option_value",
+                  "product_attribute_group", "product_attribute_name", "product_attribute_value", "kdv"
+                  ];
+
 
                 foreach ($fields as $field) {
                     $fieldArray = Parcala($value[$field]);
