0265b9db7fcbbccbf6e7630433c9ebb79a35e05d
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-11 19:43:08 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -29,7 +29,7 @@ foreach ($Supplier as $key => $value) {
 
             foreach ($valueUrun as $keyUrunIC => $valueUrunIC) {
                 $product_name = Parcala($value["kdv"]);
-
+                echo $product_name;
                 if (count($product_name) == 1) {
                   // Her ürün adını ayrı bir dizi elemanı olarak ekleyin
                   $Urunler["product_name"] = $valueUrunIC[$product_name[0]];
