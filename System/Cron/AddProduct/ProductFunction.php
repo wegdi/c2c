a989a74cbad102f358acfc319a6d1e3f4b4726d4
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-11 18:06:55 +0300

    Update ProductFunction.php

--- System/Cron/AddProduct/ProductFunction.php
+++ System/Cron/AddProduct/ProductFunction.php
@@ -13,37 +13,12 @@ class ProductJsonDecoder {
         }
     }
 
-    public function ProductJsonLoginEnd($value = '')
-    {
-        $explode = explode(';', $value);
-        if ($explode) {
-            $count = end($explode);
-            return $count;
-        } else {
-            return 0;
-        }
-    }
 
-    public function firtDelete($value = '')
-    {
-        $explode = [];
-
-        if (isset($value)) {
-            $explode = explode(';', $value);
-
-            if (count($explode) > 2) {
-                array_shift($explode);
-            }
-        }
-
-        return $explode;
-    }
 
 
 
     public function ReturnProduct($url = '', $modelv = '', $istek = '',$SupplierId='',$gettotal)
     {
-        $model = $this->ProductJsonLoginCount($modelv);
 
         $jsonData = file_get_contents($url);
         $decodedData = json_decode($jsonData, true);
