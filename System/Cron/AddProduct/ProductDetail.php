5a5f630494ab0d7645af351557899e36dea19035
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-11 18:24:07 +0300

    Update ProductDetail.php

--- System/Cron/AddProduct/ProductDetail.php
+++ System/Cron/AddProduct/ProductDetail.php
@@ -12,30 +12,14 @@ $Supplier = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 
 foreach ($Supplier as $key => $value) {
 
-  $dizi=[
-    'product_name' => $value["product_name"],
-    'product_description' => $value["product_description"],
-    'product_meta_description' => $value["product_meta_description"],
-    'product_meta_keyword'  => $value["product_meta_keyword"],
-    'quantity' => $value["quantity"],
-    'price' => $value["price"],
-    'kdv'  => $value["kdv"],
-    'model' =>  $value["model"]
-  ];
 
-  $Products=$Product->ReturnProduct(URL.$value["SupplierFilePath"],$value["star"],$dizi,$value["SupplierCode"],$_GET["page"]);
+    $jsonData = file_get_contents(URL.$value["SupplierFilePath"]);
+    $decodedData = json_decode($jsonData, true);
+    $explode = explode(';', $value["star"]);
+    PRİNT_R($explode);
 
-  foreach ($Products as $keyc => $valuec) {
-  //$ProductIf = $db->Query('Products', ["model" => (string)$valuec["model"]], [], 'TEK');
 
-  /* if ($ProductIf["_id"]=="") {
-      $db->Add("Products", $valuec);
-    }else {
-      $db->UpdateByObjectId("Products",(string)$ProductIf["_id"], $valuec);
 
-    } */
-
-  }
 
 
 }
