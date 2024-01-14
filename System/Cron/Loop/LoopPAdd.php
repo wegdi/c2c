514d2671fac42f48bf8cbfd64ca545f3c9733daa
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:27:37 +0300

    Update LoopPAdd.php

--- System/Cron/Loop/LoopPAdd.php
+++ System/Cron/Loop/LoopPAdd.php
@@ -9,7 +9,7 @@ foreach ($valueX as $key => $value) {
 
   for ($page = 1; $page <= $value["Loop"]; $page++) {
 
-      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?SupplierCode=" . $value["SupplierCode"].'?page='.$page;
+      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?SupplierCode=" . $value["SupplierCode"].'&page='.$page;
       file_get_contents($url);
 
   }
