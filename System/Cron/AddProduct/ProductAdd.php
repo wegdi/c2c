f0ae9ce4f74d35805f6fa0912ddc093107c91a23
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:28:05 +0300

    Update ProductAdd.php

--- System/Cron/AddProduct/ProductAdd.php
+++ System/Cron/AddProduct/ProductAdd.php
@@ -5,7 +5,7 @@ require_once(SYSTEM . 'General/General.php');
 $db = new General();
 
 $SupplierCode=$_GET["SupplierCode"];
-$Pages=$_GET["Page"];
+$Pages=$_GET["page"];
 
 
 $jsonFilePath = $_SERVER['DOCUMENT_ROOT'] . '/System/Product/Json/'.$SupplierCode.'_product_'.$Pages.'.json';
