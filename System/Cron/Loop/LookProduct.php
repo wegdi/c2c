1cb078be770855821a3ac234c28edb1872aff62d
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:05:16 +0300

    Update LookProduct.php

--- System/Cron/Loop/LookProduct.php
+++ System/Cron/Loop/LookProduct.php
@@ -2,7 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
 $db = new General();
-$suppliers = $db->Query('Supplier', ["Status" => 1,],["sort" => ["Total" => -1]], 'TEK');
+$value = $db->Query('Supplier', ["Status" => 1,],["sort" => ["Total" => -1]], 'TEK');
 
   echo $value["Total"];
   $Total=ceil($value["Total"]/500);
