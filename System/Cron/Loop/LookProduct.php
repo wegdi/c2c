420010b1c074e5a2e669b9a250673f259d1e087b
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:19:07 +0300

    Update LookProduct.php

--- System/Cron/Loop/LookProduct.php
+++ System/Cron/Loop/LookProduct.php
@@ -6,11 +6,22 @@ $value = $db->Query('Supplier', ["Status" => 1,],["sort" => ["Total" => -1]], 'T
 
   $Total=ceil($value["Total"]/TOTAL);
 
+  $Loop=ceil($value["Loop"]/5);
+
+
   for ($page = 1; $page <= $Total; $page++) {
       $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductDetail.php?page=" . $page;
       file_get_contents($url);
 
   }
+
+
+
+  /*for ($page = 1; $page <= $Total; $page++) {
+      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductDetail.php?page=" . $page;
+      file_get_contents($url);
+
+  } */
 
 
 
