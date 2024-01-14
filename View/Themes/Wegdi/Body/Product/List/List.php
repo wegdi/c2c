c6844e910e2b8db71377b2e19805d7a0ff77327c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 02:40:29 +0300

    Update List.php

--- View/Themes/Wegdi/Body/Product/List/List.php
+++ View/Themes/Wegdi/Body/Product/List/List.php
@@ -29,6 +29,7 @@ $security->LoginControl($guvenlik);
                   <thead>
                       <tr>
                           <th style="width:10px;">Ürün Resmi</th>
+                          <th>Marka</th>
                           <th style="width:30%;">Ürün Adı</th>
                           <th style="width:10%;">Model Kodu</th>
                           <th style="width:10%;">C2C  Kodu</th>
