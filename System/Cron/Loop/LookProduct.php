cec7302fa7b119d672a59341b29c3376feab8126
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 00:55:29 +0300

    Update LookProduct.php

--- System/Cron/Loop/LookProduct.php
+++ System/Cron/Loop/LookProduct.php
@@ -1,12 +1,29 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require_once(SYSTEM . 'General/General.php');
+require_once('ProductFunction.php');
 
-for ($page = 0; $page <= 25; $page++) {
-    $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductDetail.php?page=" . $page;
+$db = new General();
+$Product = new ProductJsonDecoder();
 
-    // Send a GET request to trigger the page
-    file_get_contents($url);
 
+$suppliers = $db->Query('Supplier', ["Status" => 1], [], 'COK');
+
+foreach ($suppliers as $key => $value) {
+
+  $Total=ceil($value/500);
+
+  for ($page = 0; $page <= $Total; $page++) {
+      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductDetail.php?page=" . $page;
+
+      // Send a GET request to trigger the page
+      file_get_contents($url);
+
+
+  }
 
 }
+
+
 
  ?>
