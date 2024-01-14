70b58147b03bd00b9af65e467683c9809c0009ed
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 02:48:51 +0300

    Update List.php

--- Modal/Product/List/List.php
+++ Modal/Product/List/List.php
@@ -17,7 +17,7 @@ $searchValue = $_POST['search']['value'];
 $filter = [];
 // Define your filtering criteria based on the DataTables search value ($searchValue)
 
-$Products = $db->Query('Products', $filter, [], 'COK', $start, $length);
+$Products = $db->Query('Products', $filter, ['sort' => ['product_meta_description' => -1]], 'COK', $start, $length);
 
 $Log = array();
 foreach ($Products as $ProductsGet) {
