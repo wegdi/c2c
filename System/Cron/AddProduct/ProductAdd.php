8d8f931f6510e84e03a284e6c4b877b7d160b332
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:14:41 +0300

    Update ProductAdd.php

--- System/Cron/AddProduct/ProductAdd.php
+++ System/Cron/AddProduct/ProductAdd.php
@@ -5,7 +5,19 @@ require_once(SYSTEM . 'General/General.php');
 $db = new General();
 
 $SupplierCode=$_GET["SupplierCode"];
+
+
 $Supplier = $db->Query('Supplier', ["Status" => 1,"SupplierCode" => $SupplierCode], [], 'TEK');
+
+
+
+for ($page = 1; $page <= $Supplier["Loop"]; $page++) {
+
+    $jsonFilePath = $_SERVER['DOCUMENT_ROOT'] . '/System/Product/Json/'.$SupplierCode.'_product_'.$_page.'.json';
+    echo $jsonFilePath;
+    //file_get_contents($url);
+
+}
 
 
 ?>
