8c058d501c6001ef37b484056beb3d0cb412cb41
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:48:34 +0300

    Update LoopPAdd.php

--- System/Cron/Loop/LoopPAdd.php
+++ System/Cron/Loop/LoopPAdd.php
@@ -1,6 +1,7 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
+
 $db = new General();
 $valueX = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 
@@ -8,8 +9,10 @@ foreach ($valueX as $key => $value) {
     for ($page = 1; $page <= $value["Loop"]; $page++) {
         $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?SupplierCode=" . $value["SupplierCode"] . '&page=' . $page;
 
-        // Talebi gönder ve sonucu kontrol etmeden devam et
-        $context = stream_context_create(['http' => ['ignore_errors' => true]]);
+        // Stream context oluştur ve timeout'u ayarla (örnekte 1 saniye)
+        $context = stream_context_create(['http' => ['timeout' => 1]]);
+
+        // Cevap beklemeden isteği gönder
         file_get_contents($url, false, $context);
     }
 }
