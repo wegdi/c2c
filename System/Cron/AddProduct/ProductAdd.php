58e351ad2d1b39a82f9918c6f063cd6768c1f96f
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:30:09 +0300

    Update ProductAdd.php

--- System/Cron/AddProduct/ProductAdd.php
+++ System/Cron/AddProduct/ProductAdd.php
@@ -4,15 +4,22 @@ require_once(SYSTEM . 'General/General.php');
 
 $db = new General();
 
-$SupplierCode=$_GET["SupplierCode"];
-$Pages=$_GET["page"];
+$SupplierCode = $_GET["SupplierCode"];
+$Pages = $_GET["page"];
 
+$jsonFilePath = $_SERVER['DOCUMENT_ROOT'] . '/System/Product/Json/' . $SupplierCode . '_product_' . $Pages . '.json';
 
-$jsonFilePath = $_SERVER['DOCUMENT_ROOT'] . '/System/Product/Json/'.$SupplierCode.'_product_'.$Pages.'.json';
+// Dosyanın var olup olmadığını kontrol et
+if (file_exists($jsonFilePath)) {
+    // Dosya varsa içeriği oku ve ekrana yazdır
+    $jsonData = file_get_contents($jsonFilePath);
+    $decodedData = json_decode($jsonData, true);
 
-$jsonData = file_get_contents($jsonFilePath);
-$decodedData = json_decode($jsonData, true);
-print_r($decodedData);
-
-
+    foreach ($decodedData as $key => $value) {
+      print_r($value);
+    }
+} else {
+    // Dosya yoksa hata mesajını yazdır
+    echo "Belirtilen dosya bulunamadı.";
+}
 ?>
