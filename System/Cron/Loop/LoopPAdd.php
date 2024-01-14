deb9f1cac60bdae836a940532dbee9486d944d32
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:24:09 +0300

    Update LoopPAdd.php

--- System/Cron/Loop/LoopPAdd.php
+++ System/Cron/Loop/LoopPAdd.php
@@ -2,17 +2,18 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
 $db = new General();
-$value = $db->Query('Supplier', ["Status" => 1,],["sort" => ["Total" => -1]], 'TEK');
+$valueX = $db->Query('Supplier', ["Status" => 1,],[], 'COK');
 
 
+foreach ($valueX as $key => $value) {
 
   for ($page = 1; $page <= $value["Loop"]; $page++) {
 
-      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?page=" . $page;
+      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?SupplierCode=" . $page;
       file_get_contents($url);
 
   }
-
+}
 
 
 
