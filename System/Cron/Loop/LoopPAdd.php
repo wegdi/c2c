8fa585ad11a7f7b95f145691da5a0c59e5fdadd4
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 02:43:02 +0300

    Update LoopPAdd.php

--- System/Cron/Loop/LoopPAdd.php
+++ System/Cron/Loop/LoopPAdd.php
@@ -9,11 +9,9 @@ foreach ($valueX as $key => $value) {
     for ($page = 1; $page <= $value["Loop"]; $page++) {
         $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?SupplierCode=" . $value["SupplierCode"] . '&page=' . $page;
 
-        // Stream context oluştur ve timeout'u ayarla (örnekte 1 saniye)
-        $context = stream_context_create(['http' => ['timeout' => 1]]);
 
         // Cevap beklemeden isteği gönder
-        file_get_contents($url, false, $context);
+        file_get_contents($url);
     }
 }
 ?>
