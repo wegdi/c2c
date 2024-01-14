fb08f13fa2bd28df59cbd062f6e4b1d863c1ab46
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:23:14 +0300

    Update LoopPAdd.php

--- System/Cron/Loop/LoopPAdd.php
+++ System/Cron/Loop/LoopPAdd.php
@@ -5,19 +5,15 @@ $db = new General();
 $value = $db->Query('Supplier', ["Status" => 1,],["sort" => ["Total" => -1]], 'TEK');
 
 
-  $Loop=ceil($value["Loop"]/5);
-  echo $Loop;
 
-/*
-  for ($page = 1; $page <= $Loop; $page++) {
+  for ($page = 1; $page <= $value["Loop"]; $page++) {
 
       $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductAdd.php?page=" . $page;
       file_get_contents($url);
 
   }
 
-*/
-asd
+
 
 
 
