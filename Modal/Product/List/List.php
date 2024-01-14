4daef3828e809e4d02f10af7c276d7c8c5851f18
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 02:45:52 +0300

    Update List.php

--- Modal/Product/List/List.php
+++ Modal/Product/List/List.php
@@ -47,7 +47,7 @@ foreach ($Products as $ProductsGet) {
         $ProductsGet["product_name"].' '.$metaDescriptionWarning,
         $ProductsGet["manufacturer_name"],
         $ProductsGet["model"],
-        $db->customShortHash($ProductsGet["model"]),
+        $ProductsGet["C2Cmodel"],
         $ProductsGet["quantity"],
         $status,
         $ProductsGet["Category"],
