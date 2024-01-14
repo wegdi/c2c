ef252ec66737b40627dd6926df023c863076fe25
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-15 01:11:53 +0300

    Update ProductAdd.php

--- System/Cron/AddProduct/ProductAdd.php
+++ System/Cron/AddProduct/ProductAdd.php
@@ -1,24 +1,10 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
-require_once('ProductFunction.php');
 
 $db = new General();
-$Product = new ProductJsonDecoder();
-echo rand();
 
-function Parcala($value = '')
-{
-    if (strpos($value, ';') !== false) {
-        $parcala = explode(';', $value);
-        array_shift($parcala);
-        return $parcala;
-    } else {
-        return [$value];
-    }
-}
+$Supplier = $db->Query('Supplier', ["Status" => 1], [], 'TEK');
 
-$suppliers = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 
-}
 ?>
