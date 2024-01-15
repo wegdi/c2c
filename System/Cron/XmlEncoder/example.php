8ba60b964ac8037093ec5da3c984c822d68c9f55
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-15 15:46:39 +0300

    Update example.php

--- System/Cron/XmlEncoder/example.php
+++ System/Cron/XmlEncoder/example.php
@@ -4,6 +4,53 @@ require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 require_once(SYSTEM.'General/General.php');
 $db = new General();
+
+
+$jsonFilePath = $_SERVER['DOCUMENT_ROOT'] . '/System/Cron/XmlEncoder/kategoriler.json';
+$jsonData = file_get_contents($jsonFilePath);
+$data = json_decode($jsonData, true);
+
+// Veritabanına kaydetme işlemi
+foreach ($data['kategoriler'] as $kategori) {
+    $kategori_title = $kategori['title'];
+    $uniqid = uniqid();
+    $data = array(
+        'Uniqid' => $uniqid,
+        'GroupId'=> '0',
+        'Title' => $kategori_title
+    );
+    $db->Add("Category_Menu", $data);
+
+    //
+    foreach ($kategori['alt_kategoriler'] as $alt_kategori) {
+        $alt_kategori_title = $alt_kategori['title'];
+        $uniqid2 = uniqid();
+        $data2 = array(
+            'Uniqid' => $uniqid2,
+            'GroupId'=> $uniqid,
+            'Title' => $alt_kategori_title
+        );
+        $db->Add("Category_Menu", $data2);
+
+        //
+        if(count($alt_kategori['alt_kategoriler2']) > 0){
+            foreach ($alt_kategori['alt_kategoriler2'] as $alt_kategori2) {
+                $alt_kategori2_title = $alt_kategori2['title'];
+                $uniqid3 = uniqid();
+                $data3 = array(
+                    'Uniqid' => $uniqid3,
+                    'GroupId'=> $uniqid2,
+                    'Title' => $alt_kategori2_title
+                );
+                $db->Add("Category_Menu", $data3);
+            }
+        }
+    }
+}
+
+
+
+
 /*
 //header('Content-Type: application/json; charset=utf-8');
 $curl_handle = curl_init();
