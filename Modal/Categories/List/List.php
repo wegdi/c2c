976f46f06dd7e470c875f80dc3dbaba8eacb4e70
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-16 12:40:48 +0300

    a

--- Modal/Categories/List/List.php
+++ Modal/Categories/List/List.php
@@ -2,4 +2,40 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
+
+require_once(SYSTEM.'General/General.php');
+$db = new General();
+
+
+
+// DataTables sends filter parameters in the request
+$draw = $_POST['draw'];
+$start = $_POST['start'];
+$length = $_POST['length'];
+$searchValue = $_POST['search']['value'];
+
+$filter = [];
+// Define your filtering criteria based on the DataTables search value ($searchValue)
+
+$Products = $db->Query('Category_Menu', $filter, ['sort' => ['Title' => -1]], 'COK', $start, $length);
+
+$Log = array();
+foreach ($Products as $ProductsGet) {
+
+
+    $Log[] = array(
+        $ProductsGet["Title"]
+    );
+}
+
+
+$getir = array(
+    "draw" => (int) $draw,
+    "recordsTotal" => $db->Quantity('Category_Menu'),
+    "recordsFiltered" => $db->Quantity('Category_Menu', $filter), // Here, we use the filtered data count
+    "data" => $Log
+);
+
+echo json_encode($getir);
+
 ?>
--- View/Themes/Wegdi/Body/Categories/List/List.php
+++ View/Themes/Wegdi/Body/Categories/List/List.php
@@ -3,4 +3,35 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 ?>
-x
+
+<div class="page-title-box d-sm-flex align-items-center justify-content-between">
+  <h4 class="mb-sm-0"><?php echo  $db->BarGet($param0.'/'.$param1,LANGUAGES_GET_DIL); ?></h4>
+
+  <div class="page-title-right">
+    <ol class="breadcrumb m-0">
+      <li class="breadcrumb-item"><a href="javascript: void(0);"><?php echo $Themes->Translate("TEXT_MENU_BAR"); ?></a></li>
+      <li class="breadcrumb-item active"><?php echo  $db->BarGet($param0.'/'.$param1,LANGUAGES_GET_DIL); ?></li>
+    </ol>
+  </div>
+
+</div>
+
+<div class="row">
+  <div class="col-lg-12">
+      <div class="card">
+          <div class="card-header">
+              <h5 class="card-title mb-0"><?php echo  $db->BarGet($param0.'/'.$param1,LANGUAGES_GET_DIL); ?></h5>
+          </div>
+          <div class="card-body">
+              <table id="MetaTable" class="display table table-bordered dt-responsive" style="width:100%">
+                  <thead>
+                      <tr>
+                          <th>Kategori Adı</th>
+                      </tr>
+                  </thead>
+
+              </table>
+          </div>
+      </div>
+  </div><!--end col-->
+</div><!--end row-->
