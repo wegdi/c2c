551e70e00198eace3b9d16f1443633b2a1c02d98
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 10:14:17 +0300

    Update C2c.php

--- System/Api/C2c.php
+++ System/Api/C2c.php
@@ -3,4 +3,21 @@
     require_once(SYSTEM.'General/General.php');
     $db = new General();
     echo 'test c2c';
+
+    $client = new http\Client;
+    $request = new http\Client\Request;
+
+    $request->setRequestUrl('https://mfkoto.myideasoft.com/admin-api/abandoned_carts');
+    $request->setRequestMethod('GET');
+    $request->setHeaders([
+    'Content-Type' => 'application/json',
+    'Authorization' => ''
+    ]);
+
+    $client->enqueue($request)->send();
+    $response = $client->getResponse();
+
+    echo $response->getBody();
+
+
 ?>
