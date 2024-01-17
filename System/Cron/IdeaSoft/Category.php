d81a05536cf88a0513d4961f37e78e89bb6e3f3b
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 15:57:34 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -2,7 +2,76 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
     require_once(SYSTEM.'General/General.php');
     $db = new General();
-    echo $db->IdeaSoftToken();
+    //echo $db->IdeaSoftToken();
+    $magaza = 'mfkoto';
+    $token = $db->IdeaSoftToken();
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode([
+        'id' => 123,
+        'name' => 'Kırtasiye',
+        'slug' => 'kirtasiye',
+        'sortOrder' => 999,
+        'status' => 0,
+        'distributor' => '',
+        'percent' => 1,
+        'imageFile' => 'kalem.jpg',
+        'displayShowcaseContent' => 0,
+        'showcaseContent' => 'Üst içerik metni.',
+        'showcaseContentDisplayType' => 1,
+        'displayShowcaseFooterContent' => 0,
+        'showcaseFooterContent' => 'string',
+        'showcaseFooterContentDisplayType' => 1,
+        'hasChildren' => 0,
+        'pageTitle' => 'string',
+        'metaDescription' => 'Kaliteli kırtasiye ürünleri.',
+        'metaKeywords' => 'kırmızı, kalem, kırtasiye',
+        'canonicalUrl' => 'kategoriler/idea-kalem',
+        'attachment' => 'string',
+        'parent' => [
+            'property1' => [
+                    'category'
+            ],
+            'property2' => [
+                    'category'
+            ]
+        ],
+        'isCombine' => 0,
+        'seoSetting' => [
+            'property1' => [
+                    'category'
+            ],
+            'property2' => [
+                    'category'
+            ]
+        ]
+    ]),
+    CURLOPT_HTTPHEADER => [
+        "Accept: application/json",
+        "Authorization: $token",
+        "Content-Type: application/json"
+    ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+    echo $response;
+    }
 
     /*
     $filter = ['GroupId' => '0'];
