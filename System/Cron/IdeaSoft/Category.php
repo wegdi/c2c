cc88de02b0adc5168070b37d0a4f03f46e2b84ab
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 16:41:17 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -16,7 +16,6 @@
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
     CURLOPT_POSTFIELDS => json_encode([
-        'id' => 1234,
         'name' => 'Kırtasiye2',
         'slug' => 'kirtasiye2',
         'sortOrder' => 999,
