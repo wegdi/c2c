18e83bca5373b0b3c571696a9d84b6a0f3eff4aa
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-17 10:46:11 +0300

    Update TokenRedirect.php

--- System/Cron/IdeaSoft/TokenRedirect.php
+++ System/Cron/IdeaSoft/TokenRedirect.php
@@ -14,7 +14,10 @@ $Response = array(
   'state' => $state,
   'code' => $code,
   'domain' => $domain,
-
+  'client_id' => '1iydisrb33pc88ccog88wgw8gwkwkc8k4woo4s8goss44koog8',
+  'response_type' => 'code',
+  'state' => '3lhhwkqmlc6cow88wgwwkwcc8k00gwsw8k8osg00084ossc4wo',
+  'redirect_uri' => 'https://c2c.wegdi.com/System/Cron/IdeaSoft/TokenRedirect.php'
 );
 
 $db->UpdateByObjectId("IdeaSoft","65a784f66b188048239f446c", $Response);
