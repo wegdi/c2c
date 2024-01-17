4d7bd27c48628f7fe4829ba6af98d53010214710
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 16:34:28 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -6,60 +6,60 @@
     $magaza = 'mfkoto';
     $token = $db->IdeaSoftToken();
 
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode([
+        'id' => 123,
+        'name' => 'Kırtasiye',
+        'slug' => 'kirtasiye',
+        'sortOrder' => 999,
+        'status' => 1,
+        'distributor' => '',
+        'percent' => 1,
+        'displayShowcaseContent' => 0,
+        'showcaseContent' => '',
+        'showcaseContentDisplayType' => 1,
+        'displayShowcaseFooterContent' => 0,
+        'showcaseFooterContent' => 'string',
+        'showcaseFooterContentDisplayType' => 1,
+        'hasChildren' => 0,
+        'pageTitle' => 'string',
+        'metaDescription' => 'Kaliteli kırtasiye ürünleri.',
+        'metaKeywords' => 'kırmızı, kalem, kırtasiye',
+        'canonicalUrl' => 'kategoriler/idea-kalem',
+        'attachment' => 'string'
+    ]),
+    CURLOPT_HTTPHEADER => [
+        "Accept: application/json",
+        "Authorization: $token",
+        "Content-Type: application/json"
+    ],
+    ]);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+        $menu = json_decode($response,true);
+        echo $menu["id"];
 
-    
+    }
+
+    /*
     $filter = ['GroupId' => '0'];
     $Category_Menu = $db->Query('Category_Menu', $filter, [], 'COK');
     foreach ($Category_Menu as $Category_Menu_Item) {
         //$Category_Menu_Item["Title"];
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-        CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode([
-            'id' => $Category_Menu_Item["Uniqid"],
-            'name' => $Category_Menu_Item["Title"],
-            'slug' => $Category_Menu_Item["Title"],
-            'sortOrder' => 999,
-            'status' => 1,
-            'distributor' => '',
-            'percent' => 1,
-            'displayShowcaseContent' => 0,
-            'showcaseContent' => '',
-            'showcaseContentDisplayType' => 1,
-            'displayShowcaseFooterContent' => 0,
-            'showcaseFooterContent' => 'string',
-            'showcaseFooterContentDisplayType' => 1,
-            'hasChildren' => 0,
-            'pageTitle' => 'string',
-            'metaDescription' => $Category_Menu_Item["Title"],
-            'metaKeywords' => $Category_Menu_Item["Title"],
-            'canonicalUrl' => $Category_Menu_Item["Title"],
-            'attachment' => 'string'
-        ]),
-        CURLOPT_HTTPHEADER => [
-            "Accept: application/json",
-            "Authorization: $token",
-            "Content-Type: application/json"
-        ],
-        ]);
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        if ($err) {
-        echo "cURL Error #:" . $err;
-        } else {
-            $menu = json_decode($response,true);
-            echo $menu["id"];
 
-        }
         //2.kategori
-        /*
         $filter = ['GroupId' => (string)$Category_Menu_Item["Uniqid"]];
         $Category_Menu2 = $db->Query('Category_Menu', $filter, [], 'COK');
         foreach ($Category_Menu2 as $Category_Menu_Item2) {
@@ -71,6 +71,6 @@
                 //$Category_Menu_Item3["Title"];
 
             }
-        }*/
-    } 
+        }
+    } */
 ?>
