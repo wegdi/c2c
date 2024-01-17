4924146f94ac6186e964d176ef6afc2abd5feab7
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 16:21:24 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -24,7 +24,7 @@
         'status' => 0,
         'distributor' => '',
         'percent' => 1,
-        'imageFile' => 'kalem.jpg',
+        'imageFile' => '//ideacdn.net/idea/ng/71/myassets/products/380/org-638327290753807461_min.jpg?revision=1704460290',
         'displayShowcaseContent' => 0,
         'showcaseContent' => 'Üst içerik metni.',
         'showcaseContentDisplayType' => 1,
