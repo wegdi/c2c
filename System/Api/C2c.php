d5c29e12ed0e16ffa1d4311c8efeb5240af8405c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-17 15:43:19 +0300

    Update C2c.php

--- System/Api/C2c.php
+++ System/Api/C2c.php
@@ -2,8 +2,7 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
     require_once(SYSTEM.'General/General.php');
     $db = new General();
-    $filter = ['client_id' => ''];
-    $IdeaSoft = $db->Query('IdeaSoft', $filter, [], 'TEK');
+    $IdeaSoft = $db->Query('IdeaSoft',['_id' => $db->ObjectId("65a784f66b188048239f446c")], [], 'TEK');
     print_r($IdeaSoft);
 
     /*
