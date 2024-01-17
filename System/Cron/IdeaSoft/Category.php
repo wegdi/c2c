30635d5781752e27e45fff54e42edf1a4a6bbede
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 17:15:30 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -6,73 +6,166 @@
     $magaza = 'mfkoto';
     $token = $db->IdeaSoftToken();
 
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => json_encode([
-        'name' => 'Menu3-3',
-        'sortOrder' => 999,
-        'status' => 1,
-        'distributor' => '',
-        'percent' => 1,
-        'displayShowcaseContent' => 0,
-        'showcaseContent' => 'Üst içerik metni.',
-        'showcaseContentDisplayType' => 1,
-        'displayShowcaseFooterContent' => 0,
-        'showcaseFooterContent' => 'string',
-        'showcaseFooterContentDisplayType' => 1,
-        'hasChildren' => 0,
-        'pageTitle' => 'Menu3',
-        'attachment' => 'string',
-        'parent' => [
-            'id' => 50590
-        ],
-    ]),
-    CURLOPT_HTTPHEADER => [
-        "Accept: application/json",
-        "Authorization: $token",
-        "Content-Type: application/json"
-    ],
-    ]);
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-    if ($err) {
-    echo "cURL Error #:" . $err;
-    } else {
-        
-        $menu = json_decode($response,true);
-        echo "<pre>";
-        print_r($menu);
-        echo "</pre>";
-        echo $menu["id"];
-
-    }
-
-    /*
+    
     $filter = ['GroupId' => '0'];
     $Category_Menu = $db->Query('Category_Menu', $filter, [], 'COK');
     foreach ($Category_Menu as $Category_Menu_Item) {
         //$Category_Menu_Item["Title"];
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+        CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode([
+            'name' => $Category_Menu_Item["Title"],
+            'sortOrder' => 999,
+            'status' => 1,
+            'distributor' => '',
+            'percent' => 1,
+            'displayShowcaseContent' => 0,
+            'showcaseContent' => 'Üst içerik metni.',
+            'showcaseContentDisplayType' => 1,
+            'displayShowcaseFooterContent' => 0,
+            'showcaseFooterContent' => 'string',
+            'showcaseFooterContentDisplayType' => 1,
+            'hasChildren' => 0,
+            'pageTitle' => $Category_Menu_Item["Title"],
+            'attachment' => 'string'
+        ]),
+        CURLOPT_HTTPHEADER => [
+            "Accept: application/json",
+            "Authorization: $token",
+            "Content-Type: application/json"
+        ],
+        ]);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+            $menu = json_decode($response,true);
+            /*
+            $menu = json_decode($response,true);
+            echo "<pre>";
+            print_r($menu);
+            echo "</pre>";
+            echo $menu["id"];*/
 
+        }
         //2.kategori
         $filter = ['GroupId' => (string)$Category_Menu_Item["Uniqid"]];
         $Category_Menu2 = $db->Query('Category_Menu', $filter, [], 'COK');
         foreach ($Category_Menu2 as $Category_Menu_Item2) {
             //$Category_Menu_Item2["Title"];
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+            CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode([
+                'name' => $Category_Menu_Item2["Title"],
+                'sortOrder' => 999,
+                'status' => 1,
+                'distributor' => '',
+                'percent' => 1,
+                'displayShowcaseContent' => 0,
+                'showcaseContent' => 'Üst içerik metni.',
+                'showcaseContentDisplayType' => 1,
+                'displayShowcaseFooterContent' => 0,
+                'showcaseFooterContent' => 'string',
+                'showcaseFooterContentDisplayType' => 1,
+                'hasChildren' => 0,
+                'pageTitle' => $Category_Menu_Item2["Title"],
+                'attachment' => 'string',
+                'parent' => [
+                    'id' => $menu["id"]
+                ]
+            ]),
+            CURLOPT_HTTPHEADER => [
+                "Accept: application/json",
+                "Authorization: $token",
+                "Content-Type: application/json"
+            ],
+            ]);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            if ($err) {
+            echo "cURL Error #:" . $err;
+            } else {
+                $menu = json_decode($response,true);
+                /*
+                $menu = json_decode($response,true);
+                echo "<pre>";
+                print_r($menu);
+                echo "</pre>";
+                echo $menu["id"];*/
+
+            }
             //3.kategori
             $filter = ['GroupId' => (string)$Category_Menu_Item2["Uniqid"]];
             $Category_Menu3 = $db->Query('Category_Menu', $filter, [], 'COK');
             foreach ($Category_Menu3 as $Category_Menu_Item3) {
                 //$Category_Menu_Item3["Title"];
+                $curl = curl_init();
+                curl_setopt_array($curl, [
+                CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => json_encode([
+                    'name' => $Category_Menu_Item3["Title"],
+                    'sortOrder' => 999,
+                    'status' => 1,
+                    'distributor' => '',
+                    'percent' => 1,
+                    'displayShowcaseContent' => 0,
+                    'showcaseContent' => 'Üst içerik metni.',
+                    'showcaseContentDisplayType' => 1,
+                    'displayShowcaseFooterContent' => 0,
+                    'showcaseFooterContent' => 'string',
+                    'showcaseFooterContentDisplayType' => 1,
+                    'hasChildren' => 0,
+                    'pageTitle' => $Category_Menu_Item3["Title"],
+                    'attachment' => 'string',
+                    'parent' => [
+                        'id' => $menu["id"]
+                    ]
+                ]),
+                CURLOPT_HTTPHEADER => [
+                    "Accept: application/json",
+                    "Authorization: $token",
+                    "Content-Type: application/json"
+                ],
+                ]);
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+                curl_close($curl);
+                if ($err) {
+                echo "cURL Error #:" . $err;
+                } else {
+                    $menu = json_decode($response,true);
+                    /*
+                    $menu = json_decode($response,true);
+                    echo "<pre>";
+                    print_r($menu);
+                    echo "</pre>";
+                    echo $menu["id"];*/
 
+                }
             }
         }
-    } */
+    }
 ?>
