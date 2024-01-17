b1d8b6c3bd095c5a73d235b19f7e67af7093d397
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-17 15:53:30 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -2,8 +2,7 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
     require_once(SYSTEM.'General/General.php');
     $db = new General();
-    $IdeaSoft = $db->Query('IdeaSoft',['_id' => $db->ObjectId("65a784f66b188048239f446c")], [], 'TEK');
-    print_r($IdeaSoft);
+    echo $db->IdeaSoftToken;
 
     /*
     $filter = ['GroupId' => '0'];
