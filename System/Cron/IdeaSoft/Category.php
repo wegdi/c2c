0ba0a10c6732b89dea4d3b4c45db70bd2d5b8161
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-18 19:23:52 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -99,7 +99,7 @@
                 $say = $say+1;
             }
             //kategori2
-            /*
+            
             $filter = ['GroupId' => (string)$Category_Menu_Item["Uniqid"]];
             $Category_Menu2 = $db->Query('Category_Menu', $filter, [], 'COK');
             foreach ($Category_Menu2 as $Category_Menu_Item2) {
@@ -202,7 +202,7 @@
                     }
                 }
                 $seflink = $db->Seflink($Category_Menu_Item["Title"]);
-            }*/
+            }
         }
         $i = $i+1;
     }
