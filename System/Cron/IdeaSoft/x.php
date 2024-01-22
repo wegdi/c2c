e26eb506a8f929351e784d5e79ad4a82ac35f655
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-22 11:34:17 +0300

    Update x.php

--- System/Cron/IdeaSoft/x.php
+++ System/Cron/IdeaSoft/x.php
@@ -4,22 +4,23 @@ require_once(SYSTEM.'General/General.php');
 $db = new General();
 
 
-// Parametreleri tanımla
-$params = array(
-    'client_id' => '1iydisrb33pc88ccog88wgw8gwkwkc8k4woo4s8goss44koog8',
-    'response_type' => 'code',
-    'state' => '3lhhwkqmlc6cow88wgwwkwcc8k00gwsw8k8osg00084ossc4wo',
-    'redirect_uri' => 'https://c2c.wegdi.com/System/Cron/IdeaSoft/TokenRedirect.php'
-);
+$auth_url = "http://www.katfarlaryedekparca.com/panel/auth";
+$client_id = "1iydisrb33pc88ccog88wgw8gwkwkc8k4woo4s8goss44koog8";
+$response_type = "code";
+$state = "3lhhwkqmlc6cow88wgwwkwcc8k00gwsw8k8osg00084ossc4wo";
+$redirect_uri = "https://c2c.wegdi.com/System/Cron/IdeaSoft/TokenRedirect.php";
 
-// Parametreleri URL'ye çevir
-$queryString = http_build_query($params);
+$full_url = $auth_url . "?client_id=" . $client_id . "&response_type=" . $response_type . "&state=" . $state . "&redirect_uri=" . urlencode($redirect_uri);
 
-// Tam URL'yi oluştur
-$url = 'http://www.katfarlaryedekparca.com/oauth/v2/token?' . $queryString;
+$ch = curl_init();
 
-// Yönlendir
-header('Location: ' . $url);
-exit;
+curl_setopt($ch, CURLOPT_URL, $full_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+echo $response;
 
 ?>
