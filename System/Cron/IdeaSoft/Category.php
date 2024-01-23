27ba695c3f461bc0512fdace8964eb06f03e3f30
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 12:41:05 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -6,50 +6,14 @@
     $token = $db->IdeaSoftToken();
 
 
-    $id_box = array('57220','57643','57776','58814','60041','60629','61002','61229','61485','62285','63003','63509','63902','64071','64223');
-    /*
-    $ids = implode(",",$id_box);
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories?ids=$ids&limit=100",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => [
-        "Accept: application/json",
-        "Authorization: $token"
-    ],
-    ]);
-
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-
-    curl_close($curl);
-
-    if ($err) {
-    echo "cURL Error #:" . $err;
-    } else {
-        $men = json_decode($response, true);
-        echo '<pre>';
-        print_r($men);
-        echo '</pre>';
-        
-
-
-
-
-    }
-    */
-
-
-    $i = 0;
-    while($i < count($id_box)){
+    $i = 1;
+    $IdeaSoftCategory = $db->Query('IdeaSoftCategory', $filter, [], 'COK', $start, $length);
+    foreach ($IdeaSoftCategory as $value) {
         $uniqid = uniqid();
-        if($i == 0){
-            $ids = $id_box[$i];
+        if($i == 1){
+            //buraya
+            $r = 0;
+            $ids = $value["IdeaSoftId"];
             $curl = curl_init();
             curl_setopt_array($curl, [
             CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories?parent=$ids&limit=100",
@@ -64,36 +28,35 @@
                 "Authorization: $token"
             ],
             ]);
-        
             $response = curl_exec($curl);
             $err = curl_error($curl);
-        
             curl_close($curl);
-        
             if ($err) {
             echo "cURL Error #:" . $err;
             } else {
                 $men = json_decode($response, true);
-                echo '<pre>';
-                print_r($men);
-                echo '</pre>';
+                //echo '<pre>';
+                //print_r($men);
+                //echo '</pre>';
+                if(count($men) > 0){
+                    while($r < count($men)){
+                        $Data = array(
+                            "Uniqid"    =>  $uniqid,
+                            "Name" => $men[$r]["name"],
+                            "Slug" => $men[$r]["slug"],
+                            "IdeaSoftId" => (int)$men[$r]["id"],
+                            "GroupId" => $ids,
+                            "SortOrder" => (int)$men[$r]["sortOrder"],
+                            "Status" => (int)$men[$r]["status"],
+                            "DistributorCode" =>  $men[$r]["distributorCode"],
+                            "IdeaSoftDate" =>  $men[$r]["createdAt"]
+                        );
+                        $db->Add("IdeaSoftCategory", $Data);
+                        $r = $r+1;
+                    }
+                }
             }
-            /*
-            $Data = array(
-                "Uniqid"    =>  $uniqid,
-                "Name" => $men[$i]["name"],
-                "Slug" => $men[$i]["slug"],
-                "IdeaSoftId" => (int)$men[$i]["id"],
-                "GroupId" => "0",
-                "SortOrder" => (int)$men[$i]["sortOrder"],
-                "Status" => (int)$men[$i]["status"],
-                "DistributorCode" =>  $men[$i]["distributorCode"],
-                "IdeaSoftDate" =>  $men[$i]["createdAt"]
-            );
-            $db->Add("IdeaSoftCategory", $Data);*/
         }
-
         $i = $i+1;
     }
-    
 ?>
