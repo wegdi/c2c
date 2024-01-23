8dcfd5f2330fd6bb7ab0f50340e3ea9cd4b03115
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 12:18:21 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -49,7 +49,35 @@
     while($i < count($id_box)){
         $uniqid = uniqid();
         if($i == 0){
-            echo $id_box[$i];
+            $ids = $id_box[$i];
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+            CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories?parent=$ids&limit=100",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "Accept: application/json",
+                "Authorization: $token"
+            ],
+            ]);
+        
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+        
+            curl_close($curl);
+        
+            if ($err) {
+            echo "cURL Error #:" . $err;
+            } else {
+                $men = json_decode($response, true);
+                echo '<pre>';
+                print_r($men);
+                echo '</pre>';
+            }
             /*
             $Data = array(
                 "Uniqid"    =>  $uniqid,
