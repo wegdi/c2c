b0ed8f3445c76a5965303c12b85835691eeca8c2
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 11:35:25 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -7,7 +7,7 @@
 
     $curl = curl_init();
     curl_setopt_array($curl, [
-    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories",
+    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories?limit=100",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
