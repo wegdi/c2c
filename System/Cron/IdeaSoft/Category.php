4c51b4094f38d21727114c27f84dda7da6fb1e3d
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 11:27:18 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -4,5 +4,30 @@
     $db = new General();
     $magaza = 'mfkoto';
     $token = $db->IdeaSoftToken();
-    echo 'test';
+
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories/tree",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "Accept: application/json",
+        "Authorization: $token"
+    ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+    echo $response;
+    }
 ?>
