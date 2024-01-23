ed8f269acd60817584c1f9389bdd47eedb55a7b1
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 12:50:32 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -4,7 +4,6 @@
     $db = new General();
     $magaza = 'mfkoto';
     $token = $db->IdeaSoftToken();
-    echo 'test';
 
     function categories_add(){
         /*$r = 0;
@@ -55,7 +54,7 @@
     
     $i = 1;
     $filter = ['GroupId' => '0'];
-    $IdeaSoftCategory = $db->Query('IdeaSoftCategory', $filter, [], 'COK', '', '');
+    $IdeaSoftCategory = $db->Query('IdeaSoftCategory', $filter, [], 'COK', '0', '');
     foreach ($IdeaSoftCategory as $value) {
         echo $value["Name"];
         $uniqid = uniqid();
