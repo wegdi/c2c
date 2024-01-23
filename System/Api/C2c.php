3ef5926938d257f841b5976303b547624e724190
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 11:24:17 +0300

    Update C2c.php

--- System/Api/C2c.php
+++ System/Api/C2c.php
@@ -4,25 +4,4 @@
     $db = new General();
     $IdeaSoft = $db->Query('IdeaSoft',['_id' => $db->ObjectId("65a784f66b188048239f446c")], [], 'TEK');
     print_r($IdeaSoft);
-
-    /*
-    $filter = ['GroupId' => '0'];
-    $Category_Menu = $db->Query('Category_Menu', $filter, [], 'COK');
-    foreach ($Category_Menu as $Category_Menu_Item) {
-        //$Category_Menu_Item["Title"];
-
-        //2.kategori
-        $filter = ['GroupId' => (string)$Category_Menu_Item["Uniqid"]];
-        $Category_Menu2 = $db->Query('Category_Menu', $filter, [], 'COK');
-        foreach ($Category_Menu2 as $Category_Menu_Item2) {
-            //$Category_Menu_Item2["Title"];
-            //3.kategori
-            $filter = ['GroupId' => (string)$Category_Menu_Item2["Uniqid"]];
-            $Category_Menu3 = $db->Query('Category_Menu', $filter, [], 'COK');
-            foreach ($Category_Menu3 as $Category_Menu_Item3) {
-                //$Category_Menu_Item3["Title"];
-
-            }
-        }
-    } */
 ?>
