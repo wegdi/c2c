f5e83b0d3198a83502f3879e93849014ddbedc1b
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 11:25:08 +0300

    Update C2cCategory.php

--- System/Api/C2cCategory.php
+++ System/Api/C2cCategory.php
@@ -3,5 +3,4 @@
     require_once(SYSTEM.'General/General.php');
     $db = new General();
     $IdeaSoft = $db->Query('IdeaSoft',['_id' => $db->ObjectId("65a784f66b188048239f446c")], [], 'TEK');
-    print_r($IdeaSoft);
 ?>
