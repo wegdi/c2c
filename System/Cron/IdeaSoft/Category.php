12c4ec0b34c46ce7e74f37066e4cd58b3d5ae618
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 13:53:39 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -113,7 +113,7 @@
     $filter = ['GroupId' => '0'];
     $IdeaSoftCategory = $db->Query('IdeaSoftCategory', $filter, [], 'COK');
     foreach ($IdeaSoftCategory as $value) {
-        if($i == 7){
+        if($i == 8){
             $ideasoftid = $value["IdeaSoftId"];
             //echo categories_add($ideasoftid,$magaza,$token);
             $filter = ['GroupId' => $ideasoftid];
