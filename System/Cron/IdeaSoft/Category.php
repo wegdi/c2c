5bd4730f833d99e84fa89598227191944049bc95
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-23 13:03:40 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -66,6 +66,13 @@
         if($i == 1){
             $ideasoftid = $value["IdeaSoftId"];
             echo categories_add($ideasoftid,$magaza,$token);
+            $filter = ['GroupId' => $ideasoftid];
+            $IdeaSoftCategory = $db->Query('IdeaSoftCategory', $filter, [], 'COK');
+            foreach ($IdeaSoftCategory as $value) {
+                $ideasoftid = $value["IdeaSoftId"];
+                echo categories_add($ideasoftid,$magaza,$token);
+            }
+            
         }
         $i = $i+1;
     }
