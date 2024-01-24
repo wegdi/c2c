16ee20966bf7ed303017a4d5e99d6f15aeef97bf
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-24 12:32:57 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -7,34 +7,34 @@
 
 
 
-$curl = curl_init();
+    $curl = curl_init();
 
-curl_setopt_array($curl, [
-  CURLOPT_URL => "https://".$magaza.".myideasoft.com/admin-api/categories/tree",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => [
-    "Accept: application/json",
-    "Authorization: Bearer $token"
-  ],
-]);
+    curl_setopt_array($curl, [
+      CURLOPT_URL => "https://".$magaza.".myideasoft.com/admin-api/categories/tree",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => [
+        "Accept: application/json",
+        "Authorization: Bearer $token"
+      ],
+    ]);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-curl_close($curl);
+    curl_close($curl);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-  $IdeaSoftCategory=json_decode($response,1);
-  print_r($IdeaSoftCategory);
-}
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+      echo $response;
+      $IdeaSoftCategory=json_decode($response,1);
+      print_r($IdeaSoftCategory);
+    }
 
 
 
