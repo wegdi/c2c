cfe206c20078c47190f165056d4f7d1daa8e5500
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-24 13:16:21 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -10,7 +10,7 @@
     $curl = curl_init();
 
     curl_setopt_array($curl, [
-      CURLOPT_URL => "https://".$magaza.".myideasoft.com/admin-api/categories",
+      CURLOPT_URL => "https://".$magaza.".myideasoft.com/admin-api/categories?limit=100",
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => "",
       CURLOPT_MAXREDIRS => 10,
