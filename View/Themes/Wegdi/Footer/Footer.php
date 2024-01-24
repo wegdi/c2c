f9b2d28f89bbd1563c4b4e7ecad4ffa88382340c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-24 15:55:10 +0300

    Update Footer.php

--- View/Themes/Wegdi/Footer/Footer.php
+++ View/Themes/Wegdi/Footer/Footer.php
@@ -173,7 +173,12 @@ var deletetitle = "<?php echo $Themes->Translate("TEXT_DELETE_ERROR"); ?>";
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-
+<script>
+       $(document).ready(function() {
+           // select2 eklentisini etkinleştirin
+           $('#listle').select2();
+       });
+   </script>
 
 </body>
 
