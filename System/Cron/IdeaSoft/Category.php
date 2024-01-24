6f9b9be9d566a9b094fb99795a54572005d6b1b5
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-24 11:16:50 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -5,6 +5,44 @@
     $magaza = 'mfkoto';
     $token = $db->IdeaSoftToken();
 
+
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories?page=1&limit=5",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "Accept: application/json",
+        "Authorization: $token"
+    ],
+    ]);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+        $men = json_decode($response, true);
+        echo '<pre>';
+        print_r($men);
+        echo '</pre>';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     /*
     //ana kategori ekleme
     $id_box = array('57220','57643','57776','58814','60041','60629','61002','61229','61485','62285','63003','63509','63902','64071','64223');
