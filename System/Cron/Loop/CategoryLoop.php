bf87c29ce61f356e8ce05efed3c572d21c6942bc
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-24 13:30:15 +0300

    Update CategoryLoop.php

--- System/Cron/Loop/CategoryLoop.php
+++ System/Cron/Loop/CategoryLoop.php
@@ -1,6 +1,6 @@
 <?php
 
-for ($page = 1; $page <= 70; $page++) {
+for ($page = 1; $page <= 100; $page++) {
     $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Category.php?page=" . $page;
     file_get_contents($url);
 
