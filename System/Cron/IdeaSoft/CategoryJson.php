b2d5225eab253c9f837c1294463c4389089d9b6e
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-24 11:41:24 +0300

    Create CategoryJson.php

---
+++ System/Cron/IdeaSoft/CategoryJson.php
@@ -0,0 +1,10 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+    require_once(SYSTEM.'General/General.php');
+    $db = new General();
+    $IdeaSoftCategory = $db->Query('IdeaSoftCategory', [], [], 'COK');
+    foreach ($IdeaSoftCategory as $key => $value) {
+        print_r($value);
+    }
+
+?>
