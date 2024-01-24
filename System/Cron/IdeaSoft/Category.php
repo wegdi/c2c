5e10ef3869c10d24217402a0e62f0b9a1cc970d9
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-24 11:25:01 +0300

    Update Category.php

--- System/Cron/IdeaSoft/Category.php
+++ System/Cron/IdeaSoft/Category.php
@@ -28,63 +28,29 @@
         echo "cURL Error #:" . $err;
         } else {
             $men = json_decode($response, true);
-            echo '<pre>';
-            print_r($men);
-            echo '</pre>';
+            //echo '<pre>';
+            //print_r($men);
+            //echo '</pre>';
+            $i = 0;
+            while($i < count($men)){
+                $uniqid = uniqid();
+                $Data = array(
+                    "Uniqid"    =>  $uniqid,
+                    "Name" => $men[$i]["name"],
+                    "Slug" => $men[$i]["slug"],
+                    "IdeaSoftId" => $men[$i]["id"],
+                    "GroupId" => "0",
+                    "SortOrder" => (int)$men[$i]["sortOrder"],
+                    "Status" => (int)$men[$i]["status"],
+                    "DistributorCode" =>  $men[$i]["distributorCode"],
+                    "IdeaSoftDate" =>  $men[$i]["createdAt"]
+                );
+                $db->Add("IdeaSoftCategory", $Data);
+                $i = $i+1;
+            }
         }
     
     }
-
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-    CURLOPT_URL => "https://$magaza.myideasoft.com/admin-api/categories?page=2&limit=100",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => [
-        "Accept: application/json",
-        "Authorization: $token"
-    ],
-    ]);
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-    if ($err) {
-    echo "cURL Error #:" . $err;
-    } else {
-        $men = json_decode($response, true);
-        $i = 0;
-        while($i < count($men)){
-            $uniqid = uniqid();
-            $Data = array(
-                "Uniqid"    =>  $uniqid,
-                "Name" => $men[$i]["name"],
-                "Slug" => $men[$i]["slug"],
-                "IdeaSoftId" => $men[$i]["id"],
-                "GroupId" => "0",
-                "SortOrder" => (int)$men[$i]["sortOrder"],
-                "Status" => (int)$men[$i]["status"],
-                "DistributorCode" =>  $men[$i]["distributorCode"],
-                "IdeaSoftDate" =>  $men[$i]["createdAt"]
-              );
-            $db->Add("IdeaSoftCategory", $Data);
-            $i = $i+1;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
 
     /*
     //ana kategori ekleme
