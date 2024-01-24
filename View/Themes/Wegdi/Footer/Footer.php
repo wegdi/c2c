9ae49dffd125680b97f76072c9254dbf0c8e8d00
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-24 16:10:56 +0300

    Update Footer.php

--- View/Themes/Wegdi/Footer/Footer.php
+++ View/Themes/Wegdi/Footer/Footer.php
@@ -84,61 +84,10 @@ $security->LoginControl($guvenlik);
  <!-- Sweet alert init js-->
  <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/pages/sweetalerts.init.js"></script>
 
- <?php if ($param0=="Home" or $param0=="" ): ?>
-
- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
- <script>
- fetch('/Modal/Home/Query/Query.php')
-   .then(response => response.json())
-   .then(data => {
-     const ctx = document.getElementById('myChart');
-
-     const chartData = {
-       labels: data.Conversions.Labels,
-       datasets: [
-         {
-           label: data.Conversions.label, // "Dönüşümler"
-           data: data.Conversions.Data,
-           backgroundColor: 'red',
-           borderWidth: 1,
-         },
-         {
-           label: data.Click.label, // "Tıklamalar"
-           data: data.Click.Data,
-           backgroundColor: 'blue',
-           borderWidth: 1,
-         },
-         {
-           label: data.Amount.label, // "Harcanan Tutar"
-           data: data.Amount.Data,
-           backgroundColor: 'green',
-           borderWidth: 1,
-         },
-       ],
-     };
-
-     new Chart(ctx, {
-       type: 'bar',
-       data: chartData,
-       options: {
-         scales: {
-           y: {
-             beginAtZero: true,
-           },
-         },
-       },
-     });
-   })
-   .catch(error => {
-     console.error('Veri çekme hatası:', error);
-   });
-
-
- </script>
- <?php endif; ?>
-<script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/pages/ecommerce-product-checkout.init.js"></script>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 <!--select2 cdn-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.2.6/jquery.inputmask.bundle.min.js"></script>
 
@@ -169,7 +118,6 @@ var deletetitle = "<?php echo $Themes->Translate("TEXT_DELETE_ERROR"); ?>";
 <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/custom.js?rn=<?php echo rand(2222,22222222222); ?>"></script>
 <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/core.js?rn=<?php echo rand(2222,22222222222); ?>"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <script>
         $(document).ready(function() {
@@ -180,7 +128,7 @@ var deletetitle = "<?php echo $Themes->Translate("TEXT_DELETE_ERROR"); ?>";
                     url: function () {
                         return $(this).data('url');
                     },
-                    dataType: 'json',
+                    dataType: 'Json',
                     delay: 250,
                     processResults: function (data) {
                         return {
