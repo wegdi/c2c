14134af77a5529f7b323bcfbf9a13cb3c4c9420c
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-25 12:23:41 +0300

    Update Header.php

--- View/Themes/Wegdi/Header/Header.php
+++ View/Themes/Wegdi/Header/Header.php
@@ -18,23 +18,26 @@ $security->LoginControl($guvenlik);
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
 
-    <!-- Layout config Js -->
-   <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/layout.js"></script>
-   <!-- Bootstrap Css -->
-   <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-   <!-- Icons Css -->
-   <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-   <!-- App Css-->
-   <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/app.min.css" rel="stylesheet" type="text/css" />
-   <!-- custom Css-->
-   <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
-
       <!--datatable css-->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
     <!--datatable responsive css-->
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
 
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+
+
+    <!-- Layout config Js -->
+    <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="<?php echo $Themes->ThemeUrl(); ?>/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+
 
 
 
