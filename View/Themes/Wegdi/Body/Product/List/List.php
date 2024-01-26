958f704df73e68acd96d66ad448f4269a966d0b0
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-26 16:05:43 +0300

    Update List.php

--- View/Themes/Wegdi/Body/Product/List/List.php
+++ View/Themes/Wegdi/Body/Product/List/List.php
@@ -58,7 +58,7 @@ $security->LoginControl($guvenlik);
 
                 </select>
               </th>
-              <th></th>
+              <th><input type="text" id="comission" class="form-control" name="comission"></th>
               <th style="width:10%;"> <select id="Category" class="js-example-basic-single"></select></th>
               <th></th>
 
