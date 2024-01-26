9984d2fbd727ac64206d9ae3cc744abb8a159120
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-26 16:30:02 +0300

    Update Commission.php

--- Modal/Product/Commission/Commission.php
+++ Modal/Product/Commission/Commission.php
@@ -4,7 +4,17 @@ require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 
 $commission = $_POST['commission'];
+$filter = [];
 
-echo json_encode($commission);
+if($commission > 0){
+    $Products = $db->Query('Products', $filter, [], 'COK');
+    foreach ($Products as $ProductsGet) {
+        $commissionTotal = $ProductsGet["price"] + ($ProductsGet["price"] * $commission);
+        $data = array(
+            'TotalPrice'  => (string)$commissionTotal
+        );
+       $db->UpdateByObjectId("Products",(string)$ProductsGet["_id"], $data);
+    }
+}
 
 ?>
