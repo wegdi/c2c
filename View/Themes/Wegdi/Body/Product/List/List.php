fb57833d48ff01ffd52f506f27604beecfef41a0
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-26 14:59:42 +0300

    Update List.php

--- View/Themes/Wegdi/Body/Product/List/List.php
+++ View/Themes/Wegdi/Body/Product/List/List.php
@@ -57,7 +57,7 @@ $security->LoginControl($guvenlik);
 
             </tr>
             <tr>
-              <th><input type="checkbox" onclick="if (!window.__cfRLUnblockHandlers) return false; $('input[name*=\'selected\']').prop('checked', this.checked);"></th>
+              <th><input type="checkbox" onclick="$('input[name*=\'selected\']').prop('checked', this.checked);"></th>
 
               <th style="width:10px;">Ürün Resmi</th>
               <th style="width:30%;">Ürün Adı</th>
