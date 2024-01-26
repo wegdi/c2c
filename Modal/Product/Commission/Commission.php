85021968c9ad5740367f367b6ea42248ab7e4e7a
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-26 16:22:50 +0300

    Update Commission.php

--- Modal/Product/Commission/Commission.php
+++ Modal/Product/Commission/Commission.php
@@ -3,6 +3,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SECURITY.'Security.php');
 $security->LoginControl($guvenlik);
 
-echo 'testtttt';
+$commission = $_POST['commission'];
+
+echo json_encode($commission);
 
 ?>
