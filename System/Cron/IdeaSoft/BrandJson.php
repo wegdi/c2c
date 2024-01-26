b8bd68ad10e1e2db11a901fc94766f6185bd457e
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-26 13:21:33 +0300

    Update BrandJson.php

--- System/Cron/IdeaSoft/BrandJson.php
+++ System/Cron/IdeaSoft/BrandJson.php
@@ -14,7 +14,7 @@ $categories = [];
 
 // Ana kategorileri bulun
 foreach ($Products as $key => $value) {
-    if(!in_array($value['manufacturer_name'], $categories, true)){
+    if(!in_array([$value['manufacturer_name']], $categories, true)){
         $categories[] = [
             'manufacturer_name' => $value['manufacturer_name'],
             'brand_id' => $value['C2Cmodel']
