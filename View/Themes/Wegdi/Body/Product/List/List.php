e861ea5382c1a81ae1251d8d101896091c8ca901
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-27 13:23:13 +0300

    Update List.php

--- View/Themes/Wegdi/Body/Product/List/List.php
+++ View/Themes/Wegdi/Body/Product/List/List.php
@@ -81,14 +81,14 @@ $security->LoginControl($guvenlik);
 
             </tr>
             <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
               <th><input type="number" class="form-control" id="comission" name="Model"></th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
               <th></th>
               <th></th>
               <th></th>
