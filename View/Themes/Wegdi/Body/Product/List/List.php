8e9d22a20a3b2f09281a5fe623f38289d4610f69
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-27 13:30:33 +0300

    Update List.php

--- View/Themes/Wegdi/Body/Product/List/List.php
+++ View/Themes/Wegdi/Body/Product/List/List.php
@@ -68,7 +68,7 @@ $security->LoginControl($guvenlik);
             <tr>
               <th colspan="2" class="text-center">Toplu Ürün<br> Güncelleme</th>
               <th colspan="2"><input type="number" class="form-control" id="comission" name="Model" placeholder="Komisyon oranı"></th>
-              <th colspan="6"></th>
+              <th colspan="8"></th>
 
             </tr>
             <tr>
