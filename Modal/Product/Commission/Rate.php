63f383b19bce0b9707bedb2f227332838acb9938
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-27 14:22:05 +0300

    Update Rate.php

--- Modal/Product/Commission/Rate.php
+++ Modal/Product/Commission/Rate.php
@@ -1,11 +1,22 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SECURITY.'Security.php');
+$security->LoginControl($guvenlik);
+
+require_once(SYSTEM.'General/General.php');
+$db = new General();
 
 
 $pricelist=$_POST["pricelist"];
+$commission=$_POST["Oran"];
+
 
 foreach ($pricelist as $key => $value) {
-      print_r($value);
-      print_r($key);
+
+
+      $commissionTotal = $value + ($value * ($commission / 100));
+      echo $commissionTotal;
+
 }
 
  ?>
