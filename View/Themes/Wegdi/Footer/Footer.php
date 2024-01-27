60bee6a203159593f93a04ecdd87d2f691b0cfff
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-27 14:04:15 +0300

    Update Footer.php

--- View/Themes/Wegdi/Footer/Footer.php
+++ View/Themes/Wegdi/Footer/Footer.php
@@ -50,7 +50,7 @@ var deletetitle = "<?php echo $Themes->Translate("TEXT_DELETE_ERROR"); ?>";
 
 
 <!-- JAVASCRIPT -->
-<!-- 
+<!--
    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
    <script src="assets/libs/simplebar/simplebar.min.js"></script>
    <script src="assets/libs/node-waves/waves.min.js"></script>
@@ -76,7 +76,7 @@ var deletetitle = "<?php echo $Themes->Translate("TEXT_DELETE_ERROR"); ?>";
 
    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
    <script src="/View/Themes/Wegdi/Src/assets/js/pages/select2.init.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/custom.js?rn=<?php echo rand(2222,22222222222); ?>"></script>
 <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/core.js?rn=<?php echo rand(2222,22222222222); ?>"></script>
 
