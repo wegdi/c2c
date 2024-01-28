5fc902b1f72e7dc29c7f0526bbe4aefa4417c7b4
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-28 18:22:49 +0300

    Create Selecet.php

---
+++ Modal/Product/Add/Selecet.php
@@ -0,0 +1,12 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SECURITY.'Security.php');
+$security->LoginControl($guvenlik);
+
+require_once(SYSTEM.'General/General.php');
+$db = new General();
+
+
+$data = array('CategoryId' => $_POST["selectedValue"] );
+echo $db->UpdateByObjectId("Products",(string)$key, $data);
+ ?>
