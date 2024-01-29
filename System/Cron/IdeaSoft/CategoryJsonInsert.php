82c2b82ecac5a98ca59a0216791093f4d0ec5d84
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-29 14:16:12 +0300

    Create CategoryJsonInsert.php

---
+++ System/Cron/IdeaSoft/CategoryJsonInsert.php
@@ -0,0 +1,52 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+function buildCategoryTree($categories, $parentId = 0, $parentNames = [])
+{
+    $branch = [];
+
+    foreach ($categories as $category) {
+        if ($category['ParentId'] == $parentId) {
+            $currentCategory = [
+                'Name' => implode(' -> ', array_merge($parentNames, [$category['Name']])),
+                'Slug' => $category['Slug'],
+                'IdeaSoftId' => $category['IdeaSoftId'],
+            ];
+
+            $branch[] = $currentCategory;
+
+            // Alt kategorileri ekleyin
+            $subcategories = buildCategoryTree($categories, $category['IdeaSoftId'], array_merge($parentNames, [$category['Name']]));
+            $branch = array_merge($branch, $subcategories);
+        }
+    }
+
+    return $branch;
+}
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SYSTEM.'General/General.php');
+
+$db = new General();
+
+$IdeaSoftCategory = $db->Query('IdeaSoftCategory', [], [], 'COK');
+
+// Tüm kategorileri depolamak için bir dizi oluşturun
+$categories = [];
+
+// Ana kategorileri bulun
+foreach ($IdeaSoftCategory as $key => $value) {
+    $categories[] = [
+        'Name' => $value['Name'],
+        'Slug' => $value['Slug'],
+        'IdeaSoftId' => $value['IdeaSoftId'],
+        'ParentId' => $value['ParentId'],
+    ];
+}
+
+// JSON çıktısı oluşturmak için ağaç yapısını oluşturun
+$tree = buildCategoryTree($categories);
+
+echo json_encode($tree, JSON_PRETTY_PRINT);
+
+?>
