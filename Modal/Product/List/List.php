7badb64da22213b13c65ebf65f933d73dd0f3846
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-29 12:26:08 +0300

    Update List.php

--- Modal/Product/List/List.php
+++ Modal/Product/List/List.php
@@ -100,8 +100,8 @@ foreach ($Products as $ProductsGet) {
             }
         }
 
-    $CategoryId= '<a href="#categoryedit" data-product-selecet-id="'.(string)$ProductsGet["_id"].'">'.$targetCategory['Name'].'</a>';
-    $CategoryId= '<select class="js-example-basic-single d-none" data-product-selecet-id="'.(string)$ProductsGet["_id"].'" name="category[]">  </select>';
+    $CategoryId.= '<a href="#categoryedit" data-product-selecet-id="'.(string)$ProductsGet["_id"].'">'.$targetCategory['Name'].'</a>';
+    $CategoryId.= '<select class="js-example-basic-single d-none" data-product-selecet-id="'.(string)$ProductsGet["_id"].'" name="category[]">  </select>';
 
     }else {
       $CategoryId= '<select class="js-example-basic-single" data-product-selecet-id="'.(string)$ProductsGet["_id"].'" name="category[]">  </select>';
