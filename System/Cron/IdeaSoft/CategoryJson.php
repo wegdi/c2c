68879aed05b115ec6409f04cdffdb17131ed20e5
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-29 14:26:40 +0300

    Update CategoryJson.php

--- System/Cron/IdeaSoft/CategoryJson.php
+++ System/Cron/IdeaSoft/CategoryJson.php
@@ -6,6 +6,13 @@ require_once(SYSTEM.'General/General.php');
 
 $db = new General();
 
+$filtre=[];
+if (ctype_digit($_GET["search"])) {
+  $filtre["IdeaSoftId"]= (int)$_GET["search"];
+}else {
+  $filtre["Name"]= (string)$_GET["Name"];
+}
+
 $Category = $db->Query('Category', [], [], 'COK');
 $CategoryJson = [];
 
