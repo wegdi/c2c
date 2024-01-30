9e84866374e9a7098a6447879f685f502264f467
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-30 10:33:32 +0300

    Update Product-Post.php

--- System/Cron/IdeaSoft/Product-Post.php
+++ System/Cron/IdeaSoft/Product-Post.php
@@ -3,7 +3,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
 
 $db = new General();
+
 $IdeaSoft = $db->Query('IdeaSoft', [], [], 'TEK');
+$ideaSoftInstance = new IdeaSoft($IdeaSoft["domain"],$IdeaSoft["access_token"]);
+
 
 $Domain=$IdeaSoft["domain"];
 
@@ -15,150 +18,130 @@ $Products = $db->Query('Products', ['_id' => $db->ObjectId($ProductId)], [], 'TE
 $Brand = $db->Query('Brand', ['Name' => $Products["manufacturer_name"]], [], 'TEK');
 
 
-$curl = curl_init();
 
-curl_setopt_array($curl, [
-  CURLOPT_URL => $Domain."/admin-api/products",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => json_encode([
-    'name' => $Products["product_name"],
-    'slug' => $db->Seflink($Products["product_name"]),
-    'sku' => $Products["C2Cmodel"],
-    'barcode' => $Products["C2Cmodel"],
-    'stockAmount' => $Products["quantity"],
-    'price1' => $Products["price_one"],
-    'currency' => [
-        'id' => 1
 
-    ],
-    'discount' => 10,
-    'discountType' => 1,
-    'moneyOrderDiscount' => '',
-    'buyingPrice' => '',
-    'marketPriceDetail' => 'string',
-    'taxIncluded' => 0,
-    'tax' => $Products["kdv"],
-    'warranty' => 24,
-    'volumetricWeight' => 1,
-    'stockTypeLabel' => 'Piece',
-    'customShippingDisabled' => 0,
-    'customShippingCost' => 5,
-    'distributor' => 'superTedarik',
-    'hasGift' => 0,
-    'gift' => '',
-    'status' => 0,
-    'hasOption' => 0,
-    'shortDetails' => '',
-    'searchKeywords' => '',
-    'installmentThreshold' => '-',
-    'homeSortOrder' => 1,
-    'popularSortOrder' => 1,
-    'brandSortOrder' => 1,
-    'featuredSortOrder' => 1,
-    'campaignedSortOrder' => 1,
-    'newSortOrder' => 1,
-    'discountedSortOrder' => 1,
-    'categoryShowcaseStatus' => 0,
-    'midblockSortOrder' => -2147483648,
-    'pageTitle' => $Products["product_name"],
-    'metaDescription' => $Products["product_name"].' '.$Products["product_meta_keyword"],
-    'metaKeywords' => '',
-    'canonicalUrl' => '',
+$ProductPost=[
+  'name' => $Products["product_name"],
+  'slug' => $db->Seflink($Products["product_name"]),
+  'sku' => $Products["C2Cmodel"],
+  'barcode' => $Products["C2Cmodel"],
+  'stockAmount' => $Products["quantity"],
+  'price1' => $Products["price_one"],
+  'currency' => [
+      'id' => 1
 
-    'brand' => [
-        'id' =>  $Brand["BrandId"]
-    ],
-    'button' => [
-        'property1' => [
-                'product'
-        ],
-        'property2' => [
-                'product'
-        ]
-    ],
-    'countDown' => [
-        'property1' => [
-                'product'
-        ],
-        'property2' => [
-                'product'
-        ]
-    ],
-
-    'categories' => [
-        [
-            'id' => $Products["CategoryId"],
-        ],
-    ],
-    'prices' => [
-
-    ],
-    'distributors' => [
-
-    ],
-    'extraInfos' => [
-
-    ],
-    'images' => [
-
-    ],
-    'tags' => [
-
-    ],
-    'mappings' => [
-
-    ],
-    'selectionGroups' => [
-
-    ],
-    'optionGroups' => [
-
-    ],
-    'labels' => [
-
-    ],
-    'offeredProducts' => [
-
-    ],
-    'combineProducts' => [
-
-    ],
-    'customizationGroups' => [
-
-    ],
-
-    'seoSetting' => [
-        'property1' => [
-                'product'
-        ],
-        'property2' => [
-                'product'
-        ]
-    ],
-    'children' => [
-
-    ]
-  ]),
-  CURLOPT_HTTPHEADER => [
-    "Accept: application/json",
-    "Authorization: Bearer ".$IdeaSoft["access_token"],
-    "Content-Type: application/json"
   ],
-]);
+  'discount' => 10,
+  'discountType' => 1,
+  'moneyOrderDiscount' => '',
+  'buyingPrice' => '',
+  'marketPriceDetail' => 'string',
+  'taxIncluded' => 0,
+  'tax' => $Products["kdv"],
+  'warranty' => 24,
+  'volumetricWeight' => 1,
+  'stockTypeLabel' => 'Piece',
+  'customShippingDisabled' => 0,
+  'customShippingCost' => 5,
+  'distributor' => 'superTedarik',
+  'hasGift' => 0,
+  'gift' => '',
+  'status' => 0,
+  'hasOption' => 0,
+  'shortDetails' => '',
+  'searchKeywords' => '',
+  'installmentThreshold' => '-',
+  'homeSortOrder' => 1,
+  'popularSortOrder' => 1,
+  'brandSortOrder' => 1,
+  'featuredSortOrder' => 1,
+  'campaignedSortOrder' => 1,
+  'newSortOrder' => 1,
+  'discountedSortOrder' => 1,
+  'categoryShowcaseStatus' => 0,
+  'midblockSortOrder' => -2147483648,
+  'pageTitle' => $Products["product_name"],
+  'metaDescription' => $Products["product_name"].' '.$Products["product_meta_keyword"],
+  'metaKeywords' => '',
+  'canonicalUrl' => '',
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+  'brand' => [
+      'id' =>  $Brand["BrandId"]
+  ],
+  'button' => [
+      'property1' => [
+              'product'
+      ],
+      'property2' => [
+              'product'
+      ]
+  ],
+  'countDown' => [
+      'property1' => [
+              'product'
+      ],
+      'property2' => [
+              'product'
+      ]
+  ],
 
-curl_close($curl);
+  'categories' => [
+      [
+          'id' => $Products["CategoryId"],
+      ],
+  ],
+  'prices' => [
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  $ss=json_decode($response,1);
-  print_r($ss);
-}
+  ],
+  'distributors' => [
+
+  ],
+  'extraInfos' => [
+
+  ],
+  'images' => [
+
+  ],
+  'tags' => [
+
+  ],
+  'mappings' => [
+
+  ],
+  'selectionGroups' => [
+
+  ],
+  'optionGroups' => [
+
+  ],
+  'labels' => [
+
+  ],
+  'offeredProducts' => [
+
+  ],
+  'combineProducts' => [
+
+  ],
+  'customizationGroups' => [
+
+  ],
+
+  'seoSetting' => [
+      'property1' => [
+              'product'
+      ],
+      'property2' => [
+              'product'
+      ]
+  ],
+  'children' => [
+
+  ]
+];
+
+
+
+$result = $ideaSoftInstance->post($ProductPost,'products');
+$result = json_decode($result,1);
+print_r($result);
