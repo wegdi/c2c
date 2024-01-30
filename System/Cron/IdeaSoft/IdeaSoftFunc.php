c1e4e7c85d19564d01294fa18f4055ac37fa9726
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-30 15:38:07 +0300

    asd

--- System/Cron/IdeaSoft/IdeaSoftFunc.php
+++ System/Cron/IdeaSoft/IdeaSoftFunc.php
@@ -39,4 +39,36 @@ class IdeaSoft {
             return $response;
         }
     }
+
+
+
+    public function delete($post = '', $url) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $this->domain."/admin-api/".$url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "DELETE",
+            CURLOPT_HTTPHEADER => [
+                "Accept: application/json",
+                "Authorization: Bearer ".$this->accessToken,
+                "Content-Type: application/json"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return $err;
+        } else {
+            return $response;
+        }
+    }
 }
---
+++ System/Cron/IdeaSoft/Product-Delete.php
@@ -0,0 +1,14 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require_once(SYSTEM . 'General/General.php');
+require_once('IdeaSoftFunc.php');
+
+$db = new General();
+
+$IdeaSoft = $db->Query('IdeaSoft', [], [], 'TEK');
+$ideaSoftInstance = new IdeaSoft($IdeaSoft["domain"],$IdeaSoft["access_token"]);
+
+
+$Domain=$IdeaSoft["domain"];
+
+$ProductId=$_GET["ProductId"];
