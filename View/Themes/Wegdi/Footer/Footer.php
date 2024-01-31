f7af84563adf9075c218296109f3a87604c5c157
Raşit KILIÇ <rstklcc@gmail.com>
2024-01-31 19:35:29 +0300

    Update Footer.php

--- View/Themes/Wegdi/Footer/Footer.php
+++ View/Themes/Wegdi/Footer/Footer.php
@@ -56,7 +56,7 @@ var deletetitle = "<?php echo $Themes->Translate("TEXT_DELETE_ERROR"); ?>";
    <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/libs/node-waves/waves.min.js"></script>
    <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/libs/feather-icons/feather.min.js"></script>
    <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
-   <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/plugins.js"></script>
+   <!-- <script src="<?php echo $Themes->ThemeUrl(); ?>/assets/js/plugins.js"></script> -->
 
 
    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
