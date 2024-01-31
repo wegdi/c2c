d25f540079d0948570193923d7280470665d1757
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-31 14:47:22 +0300

    asd

--- System/Cron/IdeaSoft/IdeaSoftFunc.php
+++ System/Cron/IdeaSoft/IdeaSoftFunc.php
@@ -41,6 +41,38 @@ class IdeaSoft {
     }
 
 
+    public function put($post = '', $url) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $this->domain."/admin-api/".$url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_POSTFIELDS => json_encode($post),
+            CURLOPT_HTTPHEADER => [
+                "Accept: application/json",
+                "Authorization: Bearer ".$this->accessToken,
+                "Content-Type: application/json"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return $err;
+        } else {
+            return $response;
+        }
+    }
+
+
 
     public function delete($url) {
         $curl = curl_init();
