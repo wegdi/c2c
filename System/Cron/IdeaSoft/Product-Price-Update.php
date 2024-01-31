b90fc8e489cdb2281f02e3203e31e5e6df8e9980
wegdi <134174429+wegdi@users.noreply.github.com>
2024-01-31 14:56:57 +0300

    Update Product-Price-Update.php

--- System/Cron/IdeaSoft/Product-Price-Update.php
+++ System/Cron/IdeaSoft/Product-Price-Update.php
@@ -27,13 +27,61 @@ foreach ($Products as $key => $value) {
 
   }
 
-  $ProductPost = [
-      'type' => 2,
-      'value' => $price,
 
-  ];
 
-  $result = $ideaSoftInstance->put($ProductPost,'product_prices/'.$value["IdeaSoftProductId"]);
+
+ $ProductPost=[
+   'name' => $value["product_name"],
+   'slug' => $db->Seflink($value["product_name"]),
+   'sku' => $value["C2Cmodel"],
+   'barcode' => $value["C2Cmodel"],
+   'stockAmount' => $value["quantity"],
+   'price1' => $price,
+   'currency' => [
+       'id' => 3
+
+   ],
+   'discount' => 0,
+   'discountType' => 1,
+   'moneyOrderDiscount' => '',
+   'buyingPrice' => '',
+   'marketPriceDetail' => '',
+   'taxIncluded' => 1,
+   'tax' => $value["kdv"],
+   'warranty' => 24,
+   'volumetricWeight' => 1,
+   'stockTypeLabel' => 'Piece',
+   'customShippingDisabled' => 0,
+   'customShippingCost' => 5,
+   'distributor' => 'superTedarik',
+   'hasGift' => 0,
+   'gift' => '',
+   'status' => 1,
+   'hasOption' => 0,
+   'shortDetails' => $value["product_name"],
+   'searchKeywords' => '',
+   'installmentThreshold' => '-',
+   'categoryShowcaseStatus' => 1,
+   'midblockSortOrder' => -2147483648,
+   'pageTitle' => $value["product_name"],
+   'metaDescription' => $value["product_name"].' '.$value["product_meta_keyword"],
+   'metaKeywords' => '',
+   'canonicalUrl' => '',
+   'detail' => [
+     'details' => $value["product_name"].' '.$value["product_description_1"].' '.$value["product_description_2"].' <br><br><br><br><br><br><a href="https://www.wegdi.com/" title="Dijital Ajans">Entegrasyon hizmeti wegdi.com tarafından sağlanmaktadır.</a>'
+
+   ],
+
+
+
+
+ ];
+
+
+
+
+
+  $result = $ideaSoftInstance->put($ProductPost,'products/'.$value["IdeaSoftProductId"]);
   $result = json_decode($result,1);
   print_r($result);
 }
