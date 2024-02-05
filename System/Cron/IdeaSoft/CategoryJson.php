7d5865eb466272f75e523b047a1a5ab5ed7248bf
wegdi <134174429+wegdi@users.noreply.github.com>
2024-02-05 14:04:14 +0300

    Update CategoryJson.php

--- System/Cron/IdeaSoft/CategoryJson.php
+++ System/Cron/IdeaSoft/CategoryJson.php
@@ -4,15 +4,25 @@ header('Content-Type: application/json; charset=utf-8');
 require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 require_once(SYSTEM.'General/General.php');
 
+header('Content-Type: application/json; charset=utf-8');
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SYSTEM.'General/General.php');
+
 $db = new General();
 
 $filtre = [];
 
-if (ctype_digit($_GET["search"])) {
-    $filtre["IdeaSoftId"] = (int)$_GET["search"];
+// Sanitize the search term
+$searchTerm = $_GET["search"];
+$searchTerm = preg_quote($searchTerm, '/');
+$searchTerm = htmlspecialchars($searchTerm, ENT_QUOTES, 'UTF-8');
+
+if (ctype_digit($searchTerm)) {
+    $filtre["IdeaSoftId"] = (int)$searchTerm;
 } else {
     // Using a case-insensitive regex for the "Name" field
-    $filtre["Name"] = new MongoDB\BSON\Regex($_GET["search"], 'i');
+    $filtre["Name"] = new MongoDB\BSON\Regex($searchTerm, 'i');
 }
 
 $Category = $db->Query('Category', $filtre, [], 'COK');
@@ -25,6 +35,5 @@ foreach ($Category as $key => $value) {
 }
 
 echo json_encode($CategoryJson);
-
 
 ?>
