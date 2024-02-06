3e3a84d221023aaed437b974e6ce0c41a9300b60
wegdi <134174429+wegdi@users.noreply.github.com>
2024-02-06 19:23:54 +0300

    Create DoganOtoKategori2.php

---
+++ System/Cron/IdeaSoft/DoganOtoKategori2.php
@@ -0,0 +1,39 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require_once(SYSTEM . 'General/General.php');
+
+$db = new General();
+
+$suppliers = $db->Query('Supplier', ["Status" => 1], [], 'COK');
+
+$kategoriler = [];
+foreach ($suppliers as $supplier) {
+    $filePath = $_SERVER['DOCUMENT_ROOT'] . $supplier["SupplierFilePath"];
+    $jsonData = file_get_contents($filePath);
+    $decodedData = json_decode($jsonData, true);
+
+    $itemsPerPage = 600; // Her sayfada kaç öğe gösterileceğini belirtin
+    $page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1; // Sayfa numarasını alın
+
+    $startIndex = ($page - 1) * $itemsPerPage;
+
+
+    $slicedStok = array_slice($decodedData["stok"],$startIndex, $itemsPerPage);
+    foreach ($slicedStok as $key => $value) {
+        //$kategoriler[] =$value["cokluKategori"];
+        $kategoribir = $value["marka"];
+        $kategoriiki = str_replace('-', '', $value["kategori"]) . ' Sonrası';
+        $kategoriuc = $value["model"];
+        $kategoriler[] = $kategoribir.' > '. $kategoriiki.' > Arka Takım Ve Süspansiyon'.;
+
+
+    }
+}
+
+
+sort($kategoriler);
+
+$newArray = array_unique($kategoriler);
+print_r($newArray);
+
+?>
