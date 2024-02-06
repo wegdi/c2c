2c2bdee7275433890ac21a68fb3c71bd40bbfdbe
wegdi <134174429+wegdi@users.noreply.github.com>
2024-02-06 17:59:33 +0300

    Update DoganOtoKategori.php

--- System/Cron/IdeaSoft/DoganOtoKategori.php
+++ System/Cron/IdeaSoft/DoganOtoKategori.php
@@ -4,28 +4,27 @@ require_once(SYSTEM . 'General/General.php');
 
 $db = new General();
 
-
 $suppliers = $db->Query('Supplier', ["Status" => 1], [], 'COK');
 
-$kategoriler=[];
+$kategoriler = [];
 foreach ($suppliers as $supplier) {
     $filePath = $_SERVER['DOCUMENT_ROOT'] . $supplier["SupplierFilePath"];
     $jsonData = file_get_contents($filePath);
     $decodedData = json_decode($jsonData, true);
 
-    foreach ($decodedData["stok"]  as $key => $value) {
-      $kategoribir=$value["marka"];
-      $kategoriiki=str_replace('-','', $value["kategori"]).' Sonrası';
-      $kategoriuc=$value["model"];
-      $kategoriler[]=$kategoriiki;
-
-
-
-
-
+    foreach ($decodedData["stok"] as $key => $value) {
+        $kategoribir = $value["marka"];
+        $kategoriiki = str_replace('-', '', $value["kategori"]) . ' Sonrası';
+        $kategoriuc = $value["model"];
+        $kategoriler[] = $kategoriiki;
     }
-    $newArray = array_unique($kategoriler);
-    print_r($newArray);
-
 }
+
+// Dizi elemanlarını artan düzende sıralar
+sort($kategoriler);
+
+// Tekrarlanmayan elemanları alır
+$newArray = array_unique($kategoriler);
+
+print_r($newArray);
 ?>
