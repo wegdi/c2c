6eceee38bfd3e5dcdcb6ba2211f93b287e47ce6b
wegdi <134174429+wegdi@users.noreply.github.com>
2024-02-06 18:08:13 +0300

    Update DoganOtoKategori.php

--- System/Cron/IdeaSoft/DoganOtoKategori.php
+++ System/Cron/IdeaSoft/DoganOtoKategori.php
@@ -16,7 +16,7 @@ foreach ($suppliers as $supplier) {
         $kategoribir = $value["marka"];
         $kategoriiki = str_replace('-', '', $value["kategori"]) . ' Sonrası';
         $kategoriuc = $value["model"];
-        $kategoriler[] =$kategoriuc;
+        $kategoriler[] =$kategoriiki;
     }
 }
 
