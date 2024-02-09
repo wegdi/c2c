f699e66efc85c97e547b1f760a82e08b29d5a743
wegdi <bildirim@wegdi.com>
2024-02-10 01:19:06 +0300

    Create Category.php

---
+++ System/Cron/Loop/Category.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($page = 1; $page <= 25; $page++) {
+    $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/DoganOtoKategori2.php?page=" . $page;
+    file_get_contents($url);
+
+}
+
+ ?>
