c7f0d865ac72c70aafd940f5567f16774395a8b3
wegdi <bildirim@wegdi.com>
2024-02-10 01:17:05 +0300

    Update DoganOtoKategori2.php

--- System/Cron/IdeaSoft/DoganOtoKategori2.php
+++ System/Cron/IdeaSoft/DoganOtoKategori2.php
@@ -12,37 +12,37 @@ foreach ($suppliers as $supplier) {
     $jsonData = file_get_contents($filePath);
     $decodedData = json_decode($jsonData, true);
 
+    $itemsPerPage = 600; // Her sayfada kaç öğe gösterileceğini belirtin
+    $page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1; // Sayfa numarasını alın
 
-    foreach ($decodedData["stok"] as $key => $value) {
+    $startIndex = ($page - 1) * $itemsPerPage;
+
+
+    $slicedStok = array_slice($decodedData["stok"],$startIndex, $itemsPerPage);
+
+
+    foreach ($slicedStok as $key => $value) {
         //$kategoriler[] =$value["cokluKategori"];
         $kategoribir = $value["marka"];
         $kategoriiki = str_replace('-', '', $value["kategori"]);
         $kategoriuc = $value["model"];
         $kategoriler[] = $kategoribir.' > ' .$kategoriiki.'  > '. $kategoriuc;
 
-        //print_r($value);
-        // Kategoriyi parçala
-      /*  $parcali_kategori = explode(" ", $kategoriiki);
 
-        // Dört basamaklı bir sayı içeren kategoriyi bul
-        foreach ($parcali_kategori as $parca) {
-            // Parçada dört basamaklı bir sayı varsa ve 1960 ile 2025 arasında ise ekrana yazdır
-            if (preg_match('/\b\d{4}\b/', $parca) && $parca >= 1960 && $parca <= 2025) {
+        $Products = $db->Query('Products', ["SupplierCode" => $supplier["SupplierCode"], "model" => $value["kod"]], [], 'TEK');
 
+        if ($Products["_id"] != "") {
 
+            $value = array(
+              'CategoryOne' => $kategoribir,
+              'CategoryTwo' => $kategoriiki,
+              'CategoryTree' => $kategoriuc
 
+            );
+            $db->UpdateByObjectId("Products", (string)$Products["_id"], $value);
 
-              $metin = str_replace($bul,"Karoser İç Parçalar", $kategoriuc);
-
-              $kategoriler[] = $kategoribir.' > ' .$parcali_kategori[0].'  > '. $parca.' > '.$metin;
-
-
-                //echo "Dört basamaklı sayı bulundu ve 1960 ile 2025 arasında: $parca<br>";
-            }
         }
 
-
-*/
     }
 }
 
