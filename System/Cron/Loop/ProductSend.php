4ddff6bae73e394f9f976398221a2b0976986dcc
wegdi <bildirim@wegdi.com>
2024-02-12 02:00:27 +0300

    Create ProductSend.php

---
+++ System/Cron/Loop/ProductSend.php
@@ -0,0 +1,29 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require_once(SYSTEM . 'General/General.php');
+$db = new General();
+
+$filtre=[];
+$filtre['CategoryId']=['$ne' => null];
+
+$value = $db->Query('Products', $filtre,[], 'COK');
+
+print_r($value);
+/*
+  $Total=ceil($value["Total"]/TOTAL);
+
+
+
+  for ($page = 1; $page <= $Total; $page++) {
+      $url = "https://c2c.wegdi.com/System/Cron/AddProduct/ProductDetail.php?page=" . $page;
+      file_get_contents($url);
+
+  }
+
+
+
+*/
+
+
+
+ ?>
