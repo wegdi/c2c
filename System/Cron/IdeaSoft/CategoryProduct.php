8c22f0ef44784760ca01228f528ef6ee741de429
wegdi <bildirim@wegdi.com>
2024-02-12 01:51:31 +0300

    Update CategoryProduct.php

--- System/Cron/IdeaSoft/CategoryProduct.php
+++ System/Cron/IdeaSoft/CategoryProduct.php
@@ -4,26 +4,23 @@ require_once(SYSTEM . 'General/General.php');
 
 $db = new General();
 
-$CategoryList = $db->Query('CategoryList',[], [], 'COK');
+// Kategori listesini al
+$categories = $db->Query('CategoryList', [], [], 'COK');
 
-foreach ($CategoryList as $key => $value) {
-  if ($value["IdeaSoftId"]!="") {
+// Her bir kategori için işlem yap
+foreach ($categories as $category) {
+    $ideaSoftId = $category["IdeaSoftId"];
 
-    $Products = $db->Query('Products',['CategoryFull' => $value["CategoryFull"]], [], 'COK');
+    // Eğer IdeaSoftId boş değilse işleme devam et
+    if (!empty($ideaSoftId)) {
+        // Kategorinin ürünlerini al
+        $products = $db->Query('Products', ['CategoryFull' => $category["CategoryFull"]], [], 'COK');
 
-    foreach ($Products as $keyx => $valuex) {
-
-
-      $arrayName = array('CategoryId' => $value["IdeaSoftId"] );
-      $db->UpdateByObjectId("Products", (string)$valuex["_id"], $arrayName);
-
-
+        // Her bir ürün için işlem yap
+        foreach ($products as $product) {
+            // Ürünün IdeaSoftId alanını güncelle
+            $db->UpdateByObjectId("Products", (string)$product["_id"], ['CategoryId' => $ideaSoftId]);
+        }
     }
-
-
-
-
-
-  }
-
 }
+?>
