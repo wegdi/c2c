6255c17f5c714c1260c5321fbfdef4a73b18617c
wegdi <bildirim@wegdi.com>
2024-02-11 19:04:06 +0300

    Update CategorySave.php

--- Modal/Supplier/Category/CategorySave.php
+++ Modal/Supplier/Category/CategorySave.php
@@ -1,4 +1,27 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SECURITY.'Security.php');
+$security->LoginControl($guvenlik);
+require_once(SYSTEM.'General/General.php');
+$db=new General();
 
-print_r($_POST);
- ?>
+
+if ($_POST["selected"]!="" and $_POST["IdeaSoftId"]!="") {
+
+  foreach ($$_POST["selected"] as $key => $value) {
+    print_r($value);
+    // code...
+ /*
+  $data = array(
+    'IdeaSoftId' => (int)$_POST["IdeaSoftId"]
+  );
+
+  $response = $db->UpdateByObjectId("CategoryList",, $data);
+  */
+  }
+//  echo $response;
+
+}
+
+
+?>
