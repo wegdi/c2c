589046cf068b649afd34e9392b6452b0975f7be3
wegdi <bildirim@wegdi.com>
2024-02-11 17:36:23 +0300

    Update Category2.php

--- Modal/Supplier/Category/Category2.php
+++ Modal/Supplier/Category/Category2.php
@@ -6,17 +6,16 @@ $security->LoginControl($guvenlik);
 require_once(SYSTEM.'General/General.php');
 $db = new General();
 
-$Marka=$_POST["Marka"];
+$Marka = $_POST["Marka"];
 $uniqueCategories = []; // Benzersiz kategorileri saklamak için bir dizi oluşturuyoruz
-$CategoryList = $db->Query('CategoryList',['CategoryOne' => $Marka], [], 'COK');
+$CategoryList = $db->Query('CategoryList', ['CategoryOne' => $Marka], [], 'COK');
 
-$List=[];
-foreach ($CategoryList  as $key => $value) {
-      $List[]=$value["CategoryTwo"];
-      if (!in_array($value["CategoryTwo"], $uniqueCategories)):
+foreach ($CategoryList as $key => $value) {
+    // Her bir kategori değerini diziye eklerken, aynı zamanda benzersiz olup olmadığını kontrol ediyoruz
+    if (!in_array($value["CategoryTwo"], $uniqueCategories)) {
         $uniqueCategories[] = $value["CategoryTwo"]; // Kategoriyi benzersiz dizisine ekliyoruz
-
-
+    }
 }
 
 echo json_encode($uniqueCategories);
+?>
