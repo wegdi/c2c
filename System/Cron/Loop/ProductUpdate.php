93a830f44704a78dac9310c9c96494b2e2d5e998
wegdi <bildirim@wegdi.com>
2024-02-13 21:32:07 +0300

    Update ProductUpdate.php

--- System/Cron/Loop/ProductUpdate.php
+++ System/Cron/Loop/ProductUpdate.php
@@ -3,18 +3,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
 $db = new General();
 
-$Products = $db->Quantity('Products', ['IdeaSoft' => 1]);
+$Products = $db->Query('Products', ['_id' => $db->ObjectId($ProductId),'IdeaSoft' => 1], [], 'COK');
 
-// Örnek bir $value dizisi tanımla
-$value = array("SupplierCode" => "örnek_değer");
 
-// Çoklu cURL oturumu oluştur
-$mh = curl_multi_init();
-
-// Her sayfa için bir cURL oturumu başlat
-for ($i = 1; $i <= ceil($Products / 40); $i++) {
-    $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Update.php?page=".$i;
-
-    file_get_contents($url);
-
+foreach ($Products as $key => $value) {
+  $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Update.php?page=".(string)$value["_id"];
+  file_get_contents($url);
 }
