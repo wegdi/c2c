3ed291fc7dba46a092256cc9c8764c21e97bf839
wegdi <bildirim@wegdi.com>
2024-02-13 21:34:15 +0300

    Update ProductUpdate.php

--- System/Cron/Loop/ProductUpdate.php
+++ System/Cron/Loop/ProductUpdate.php
@@ -7,6 +7,6 @@ $Products = $db->Query('Products', ['IdeaSoft' => 1],[], 'COK');
 
 
 foreach ($Products as $key => $value) {
-  $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Update.php?page=".(string)$value["_id"];
+  $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Update.php?ProductId=".(string)$value["_id"];
   file_get_contents($url);
 }
