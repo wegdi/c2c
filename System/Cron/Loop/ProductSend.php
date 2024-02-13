22ad5b3acff24151dc59ac066f69623a70914468
wegdi <bildirim@wegdi.com>
2024-02-13 15:00:06 +0300

    Update ProductSend.php

--- System/Cron/Loop/ProductSend.php
+++ System/Cron/Loop/ProductSend.php
@@ -11,7 +11,7 @@ $filtre['IdeaSoft']=0;
 $valuex = $db->Query('Products', $filtre,[], 'COK',1,5);
 
 foreach ($valuex as $key => $value) {
-
+  print_r($value);
   $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Send.php?ProductId=" . (string)$value["_id"];
   file_get_contents($url);
 
