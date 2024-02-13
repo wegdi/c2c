338f4f51f7b752b2cd88b511693010a2f5382484
wegdi <bildirim@wegdi.com>
2024-02-13 15:39:23 +0300

    Update ProductSend.php

--- System/Cron/Loop/ProductSend.php
+++ System/Cron/Loop/ProductSend.php
@@ -7,16 +7,19 @@ $filtre=[];
 $filtre['CategoryId']=['$ne' => null];
 $filtre['IdeaSoft']=0;
 
-
 $valuex = $db->Query('Products', $filtre,[], 'COK',1,30);
 
 foreach ($valuex as $key => $value) {
   print_r($value);
   $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Send.php?ProductId=" . (string)$value["_id"];
-  file_get_contents($url);
 
+  // Curl kullanarak bir istek gönderme
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_exec($ch);
+  curl_close($ch);
 }
 
-
-
- ?>
+?>
