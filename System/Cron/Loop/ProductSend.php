9103422a757ebd9a06aa20d99e1256547a7ddfb9
wegdi <bildirim@wegdi.com>
2024-02-13 15:11:40 +0300

    Update ProductSend.php

--- System/Cron/Loop/ProductSend.php
+++ System/Cron/Loop/ProductSend.php
@@ -8,7 +8,7 @@ $filtre['CategoryId']=['$ne' => null];
 $filtre['IdeaSoft']=0;
 
 
-$valuex = $db->Query('Products', $filtre,[], 'COK');
+$valuex = $db->Query('Products', $filtre,[], 'COK',1,5);
 
 foreach ($valuex as $key => $value) {
   print_r($value);
