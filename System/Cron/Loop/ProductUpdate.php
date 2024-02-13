39c3959667a82dd4d16094640071e16b2c0f1755
wegdi <bildirim@wegdi.com>
2024-02-13 21:57:06 +0300

    Update ProductUpdate.php

--- System/Cron/Loop/ProductUpdate.php
+++ System/Cron/Loop/ProductUpdate.php
@@ -1,13 +1,8 @@
 <?php
-require_once('../../config.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(SYSTEM . 'General/General.php');
-echo LANGUAGES_GET_DIL;
 $db = new General();
 
-echo SYSTEM . 'General/General.php';
-echo "<br>";
-echo __DIR__;
-/*
 $Products = $db->Query('Products', ['IdeaSoft' => 1],[], 'COK');
 
 
@@ -15,4 +10,3 @@ foreach ($Products as $key => $value) {
   $url = "https://c2c.wegdi.com/System/Cron/IdeaSoft/Product-Price-Update.php?ProductId=".(string)$value["_id"];
   file_get_contents($url);
 }
-*/
