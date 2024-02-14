3411aa61e1e71896caac6f123c3306bf1300a66f
Raşit KILIÇ <rstklcc@gmail.com>
2024-02-14 12:29:33 +0300

    Update CategoryCreate.php

--- Modal/Supplier/Category/CategoryCreate.php
+++ Modal/Supplier/Category/CategoryCreate.php
@@ -22,32 +22,44 @@ if($Category1["_id"] != ""){
     'distributor' => '',
     'percent' => 1,
     'displayShowcaseContent' => 0,
+    'showcaseContent' => 'Üst içerik metni.',
     'showcaseContentDisplayType' => 1,
     'displayShowcaseFooterContent' => 0,
     'showcaseFooterContent' => 'string',
     'showcaseFooterContentDisplayType' => 1,
     'hasChildren' => 0,
+    'pageTitle' => 'string',
+    'metaDescription' => '',
+    'metaKeywords' => '',
+    'canonicalUrl' => '',
+    'attachment' => 'string',
     'isCombine' => 0
   ];
-  $curl = curl_init();
-  curl_setopt_array($curl, [
-      CURLOPT_URL => "https://mfkoto.myideasoft.com/admin-api/categories",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS => json_encode($data),
-      CURLOPT_HTTPHEADER => [
-          "Accept: application/json",
-          "Authorization: Bearer YzJiMTBlZDAyZDY3YTcyMzUxZDhkZGYzZWRjYTc4M2FlMGFkZmVlZDQwNWM3YTUzZGJiYjQ5M2NlOTRmYmJlYg",
-          "Content-Type: application/json"
-      ],
-  ]);
-  $response = curl_exec($curl);
-  $err = curl_error($curl);
-  curl_close($curl);
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://mfkoto.myideasoft.com/admin-api/categories",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($data),
+  CURLOPT_HTTPHEADER => [
+    "Accept: application/json",
+    "Authorization: Bearer NzQxMTI1NWZkZWQzMTlmYWQxNmYzNzg4NThkZGRlYzY1NGU3YzZhZTQxYjQzZDIwZjlkOTdiNWZjNmRmZWVmYQ",
+    "Content-Type: application/json"
+  ],
+]);
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+}
   //$response = $ideaSoftInstance->post($data,'categories');
   $response = json_decode($response,true);
   print_r($response);
@@ -62,7 +74,6 @@ if($Category1["_id"] != ""){
     );
     $result = $db->Add("IdeaSoftCategory", $dataadd);
   }
-  echo 'yok';
 }
 
 /*
