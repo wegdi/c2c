0fdb6f5402168bd529e0bdfef3783b1ef8d8159f
Raşit KILIÇ <rstklcc@gmail.com>
2024-02-15 11:52:22 +0300

    Update TokenStart.php

--- System/Cron/IdeaSoft/TokenStart.php
+++ System/Cron/IdeaSoft/TokenStart.php
@@ -1,11 +1,16 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
+require_once(SYSTEM.'General/General.php');
+$db = new General();
 
+
+$IdeaSoft = $db->Query('IdeaSoft',["_id" => $db->ObjectId('65a784f66b188048239f446c')], [], 'TEK');
 // Parametreleri bir dizi olarak oluştur
 $parameters = array(
-    'client_id' => '3pebeuh6xb40swk0c08ckkow0g0ogcc40k4ggkw8so8owgowk4',
+    'client_id' => $IdeaSoft["client_id"],
     'response_type' => 'code',
-    'state' => '40cezisrdy68cwoso8k40c4kcso040o8sso80ggkggs4gkk84c',
-    'redirect_uri' => 'https://c2c.wegdi.com/System/Cron/IdeaSoft/TokenRedirect.php'
+    'state' => $IdeaSoft["state"],
+    'redirect_uri' => $IdeaSoft["redirect_uri"]
 );
 
 // Parametreleri URL'ye eklemek için http_build_query kullan
